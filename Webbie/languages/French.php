90c13fca119014f3eefcb99e909034cc6c3e00b1
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2018-01-28 01:02:41 +0100

    Add files via upload

---
+++ Webbie/languages/French.php
@@ -0,0 +1,198 @@
+<?php
+
+//FR: Cette version du langage français à été crée par Rémi VIDON pour NM (NetworkManager)
+//EN: This version of the French language was created by Rémi VIDON for NM (NetworkManager)
+
+//FR: Dernière MAJ le 26/01/2017 par RémiVIDON (TheRemiDev sûr le discord -> Donator)
+//EN: Last updated 26/01/2017 By Rémi VIDON (TheRemiDev on Discord -> Donator)
+
+$lang = array();
+
+$lang['LANGUAGE'] = 'Français';
+$lang['AUTHOR'] = 'TheRemiDev';
+
+$lang['TITLE_OVERVIEW'] = 'Aperçu';
+$lang['TITLE_ANALYTICS'] = 'Analytique';
+
+$lang['TITLE_PLAYERS'] = 'Joueurs';
+$lang['TITLE_PUNISHMENTS'] = 'Les sanctions';
+$lang['TITLE_REPORTS'] = 'Les signalements';
+$lang['TITLE_CHAT'] = 'Chat';
+$lang['TITLE_CHATLOGS'] = 'ChatLogs';
+$lang['TITLE_HELPOP'] = 'Aide';
+$lang['TITLE_TICKETS'] = 'Les billets';
+
+$lang['TITLE_SETTINGS'] = 'Paramètres';
+$lang['TITLE_MOTD'] = 'MOTD';
+$lang['TITLE_PERMISSIONS'] = 'Permissions';
+$lang['TITLE_FILTER'] = 'Filtre';
+$lang['TITLE_COMMANDBLOCKER'] = 'CommandBlocker';
+$lang['TITLE_TABCOMPLETECOMMANDS'] = 'TabCompleteCommands';
+
+$lang['TITLE_GROUPSANDACCOUNTS'] = 'Groupes et comptes';
+
+$lang['TITLE_TASKS'] = 'Tâches';
+$lang['TITLE_ACCOUNTSETTINGS'] = 'Paramètres du compte';
+$lang['TITLE_LOGOUT'] = 'Déconnexion';
+
+$lang['TITLE_DASHBOARD'] = 'Tableau de bord';
+
+$lang['TITLE_NETWORKSETTINGS'] = 'Paramètres réseau';
+
+$lang['TITLE_MOTDANDMAINTENANCE'] = 'MOTD & Maintenance';
+
+$lang['DASHBOARD_TOTALPLAYERS'] = 'Total des joueurs';
+$lang['DASHBOARD_TODAYONLINEPLAYERS'] = 'Joueurs en ligne d''aujourd''hui';
+$lang['DASHBOARD_TODAYSNEWPLAYERS'] = 'Les nouveaux joueurs d''aujourd''hui';
+$lang['DASHBOARD_TODAYSPLAYTIME'] = 'Playtime d''aujourd''hui';
+$lang['DASHBOARD_PLAYERSTATISTICS'] = 'Statistiques des joueurs';
+$lang['DASHBOARD_NEWESTPLAYERS'] = 'Nouveaux joueurs';
+$lang['ANALYTICS_ONLINE'] = 'En ligne';
+$lang['ANALYTICS_AVERAGESTATS'] = 'Statistiques moyennes';
+$lang['ANALYTICS_MOSTPLAYEDVERSIONS'] = 'Versions les plus jouées';
+$lang['ANALYTICS_PLAYERREGIONS'] = 'Régions de joueurs';
+$lang['ANALYTICS_PLAYTIME'] = 'Temps de jeux';
+$lang['ANALYTICS_USERRETENTION'] = 'Rétention de l''utilisateur';
+$lang['ANALYTICS_FUTUREPORJECTIONS'] = 'Projections futures';
+
+$lang['PLAYER_PLAYERINFORMATION'] = 'Informations sur le joueur';
+$lang['PLAYER_PLAYERSTATISTICS'] = 'Statistiques du joueur';
+$lang['PLAYER_LATESTSESSIONS'] = 'Dernières sessions';
+$lang['PLAYER_NOTES'] = 'Notes';
+$lang['PLAYER_PUNISHMENTS'] = 'Les sanctions';
+$lang['PLAYER_USERNAME'] = 'Nom d''utilisateur';
+$lang['PLAYER_NICKNAME'] = 'Nick';
+$lang['PLAYER_UUID'] = 'UUID';
+$lang['PLAYER_COUNTRY'] = 'Pays';
+$lang['PLAYER_LATESTMINECRAFTVERSION'] = 'Dernière version Minecraft';
+$lang['PLAYER_IPADDRESS'] = 'Adresses IP';
+$lang['PLAYER_JOINED'] = 'Inscription';
+$lang['PLAYER_LASTLOGIN'] = 'Dernière connexion';
+$lang['PLAYER_LASTLOGOUT'] = 'Dernière déconnexion';
+$lang['PLAYER_ONLINE'] = 'En ligne';
+$lang['PLAYER_PLAYTIME'] = 'Temps de jeu';
+$lang['PLAYER_AVERAGEPLAYTIME'] = 'Temps de jeu moyen';
+$lang['PLAYER_JOINSAT'] = 'Le joueur se joint normalement à';
+$lang['PLAYER_ALTACCOUNTS'] = 'Comptes supplémentaires';
+$lang['PLAYER_SEARCH'] = 'Rechercher un joueur par nom, UUID ou adresse IP';
+
+$lang['PUNISHMENTS_PUNISHMENT'] = 'Sanction';
+$lang['PUNISHMENTS_TYPE'] = 'Type';
+$lang['PUNISHMENTS_PUNISHED'] = 'Joueur';
+$lang['PUNISHMENTS_PUNISHER'] = 'Par';
+$lang['PUNISHMENTS_TIME'] = 'Temps';
+$lang['PUNISHMENTS_END'] = 'Fin';
+$lang['PUNISHMENTS_SERVER'] = 'Serveur';
+$lang['PUNISHMENTS_REASON'] = 'Raison';
+$lang['PUNISHMENTS_STATUS'] = 'Status';
+$lang['PUNISHMENTS_UNBANNER'] = 'Débannisseur';
+$lang['PUNISHMENTS_ACTIVE'] = 'Actif';
+$lang['PUNISHMENTS_EXPIRED'] = 'Expiré';
+$lang['PUNISHMENTS_SEARCH'] = 'Rechercher un sanctionnement par IP, puni, punisseur ou raison';
+
+$lang['REPORTS_REPORT'] = 'Rapport'; // Ou Signaleur je ne sais plus trop. 
+$lang['REPORTS_REPORTER'] = 'Par';
+$lang['REPORTS_CLOSE'] = 'Fermer';
+$lang['REPORTS_SEARCH'] = 'Rechercher des rapports par joueur, signaleur ou raison';
+
+$lang['CHAT_SEARCH'] = 'Rechercher un message par uuid, message, serveur ou ip';
+
+$lang['NOTES_NOTE'] = 'Note';
+
+$lang['CHATLOG_CHATLOGID'] = 'Chatlog ID';
+$lang['CHATLOG_TRACKED'] = 'Suivi';
+$lang['CHATLOG_CHATLOGS'] = 'Chatlogs';
+$lang['CHATLOG_SEARCH'] = 'Rechercher dans le chatlog par créateur, uuid ou suivi';
+
+$lang['HELPOP_REQUESTER'] = 'Demandeur';
+$lang['HELPOP_SEARCH'] = 'Rechercher des demandes d''aide par demandeur, serveur ou message';
+
+$lang['TICKET_TICKET'] = 'Ticket';
+$lang['TICKET_TITLE'] = 'Titre';
+$lang['TICKET_ASSIGNEDTO'] = 'Assigné à';
+$lang['TICKET_LASTUPDATE'] = 'Dernière mise à jour';
+$lang['TICKET_ANSWER'] = 'Répondre';
+$lang['TICKET_CLOSE'] = 'Fermer';
+$lang["TICKET_SEARCH"] = 'Rechercher des tickets par créateur, titre ou assigné à';
+
+$lang['SETTINGS_SEARCH'] = 'Paramètre de recherche par variable';
+
+$lang['MOTD_MOTDGENERATOR'] = 'Générateur MOTD';
+$lang['MOTD_KICKMSG'] = 'Message d''expulsion';
+$lang['MOTD_PREVIEW'] = 'Aperçu';
+$lang['MOTD_MAINTENANCEMODE'] = 'Mode Maintenance';
+
+$lang['PERMISSIONS_EDITPERMS'] = 'Modifier les permissions';
+$lang['PERMISSIONS_EDITINHERITANCES'] = 'Modifier les héritages (Inheritances)';
+$lang['PERMISSIONS_EDITGROUPSETTINGS'] = 'Modifier les paramètres du groupe';
+$lang['PERMISSIONS_SELECTGROUP'] = 'Sélectionner un groupe';
+
+$lang['FILTER_ADDWORD'] = 'Ajouter un mot';
+
+$lang['COMMANDBLOCKER_ADDCOMMAND'] = 'Ajouter une commande';
+
+$lang['TABCOMPLETECOMMANDS_ADDCOMMAND'] = 'Ajouter une commande';
+
+$lang['GROUPANDACCOUNTS_GROUP'] = 'Groupe';
+$lang['GROUPANDACCOUNTS_USER'] = 'Utilisateur';
+
+$lang['TASKS_YOURTASKS'] = 'Vos tâches';
+
+$lang['ACCOUNTSETTINGS_OLDPASS'] = 'Ancien mot de passe';
+$lang['ACCOUNTSETTINGS_NEWPASS'] = 'Nouveau mot de passe';
+$lang['ACCOUNTSETTINGS_SELECTED_LANG'] = 'Langue sélectionnée:';
+
+$lang['LOGIN_TITLE'] = 'NetworkManager';
+$lang['LOGIN_USERNAME'] = 'Nom d''utilisateur';
+$lang['LOGIN_PASSWORD'] = 'Mot de passe';
+$lang['LOGIN_REMEMBERME'] = 'Souviens-toi de moi';
+$lang['LOGIN_LOGIN'] = 'Connexion';
+$lang['LOGIN_WRONGPASSWORD'] = 'Mot de passe invalide.';
+$lang['LOGIN_LOGGINGIN'] = 'Redirection en cours. Patientez...';
+$lang['LOGIN_NODBCONNECTION'] = 'Une erreur est survenue contactez un Administrateur (Base de données)';
+
+$lang['VAR_ID'] = 'ID';
+$lang['VAR_VIEW'] = 'Voir';
+$lang['VAR_PUNISHMENT'] = 'Sanctionnement';
+$lang['VAR_PLAYER'] = 'Pseudonyme';
+$lang['VAR_PUNISHER'] = 'Par';
+$lang['VAR_REASON'] = 'Raison';
+$lang['VAR_TIME'] = 'Temps';
+$lang['VAR_STARTED'] = 'Commencé';
+$lang['VAR_ENDED'] = 'Terminé';
+$lang['VAR_VERSION'] = 'Version';
+$lang['VAR_UNBAN'] = 'Débannir';
+$lang['VAR_DELETEBAN'] = 'Masquer le bannissement';
+$lang['VAR_UNMUTE'] = 'Démute';
+$lang['VAR_DELETEMUTE'] = 'Masquer le mute';
+$lang['VAR_DELETEKICK'] = 'Masquer le kick';
+$lang['VAR_UNWARN'] = 'Déwarn';
+$lang['VAR_DELETEWARN'] = 'Masquer le warn';
+$lang['VAR_MESSAGE'] = 'Message';
+$lang['VAR_SERVER'] = 'Serveur';
+$lang['VAR_TICKET'] = 'Ticket';
+$lang['VAR_CREATOR'] = 'Créateur';
+$lang['VAR_ON'] = 'On';
+$lang['VAR_OFF'] = 'Off';
+$lang['VAR_ICON'] = 'Icon';
+$lang['VAR_DELETE'] = 'DELETE';
+$lang['VAR_DEFAULT'] = 'Défaut';
+$lang['VAR_ACTION'] = 'Action';
+$lang['VAR_PASSWORD'] = 'Mot de passe';
+$lang['VAR_LANGUAGES'] = 'Langues';
+$lang['VAR_GLOBAL'] = 'Global';
+$lang['VAR_SAVE'] = 'Sauvegarder';
+$lang['VAR_CANCEL'] = 'Annuler';
+$lang['VAR_MONTHLY'] = 'Mensuel';
+$lang['VAR_WEEKLY'] = 'Hebdomadaire';
+$lang['VAR_DAILY'] = 'Quotidien';
+$lang['VAR_TODAY'] = 'Aujourd''hui';
+$lang['VAR_THISWEEK'] = 'Cette semaine';
+$lang['VAR_TOTAL'] = 'Total';
+$lang['VAR_LOADING'] = 'Chargement...';
+$lang['VAR_BY'] = 'Par';
+
+$lang['TEXT_NORESULTS'] = 'Aucun résultat n''a pu être affiché';
+$lang['TEXT_NORESULTS_SEARCH'] = 'Aucun résultat trouvé pour';
+$lang['TEXT_SELECTOPTION'] = ' -- Choisir une option -- ';
+$lang['TEXT_NOTASKS'] = 'Pas de tâches! Passez une bonne journée :)';
