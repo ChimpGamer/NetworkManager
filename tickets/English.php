9220c945b3a4a34d0964a356cb7d3e6abb5b7b5c
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2018-01-29 23:55:13 +0100

    Create English.php

---
+++ tickets/English.php
@@ -0,0 +1,26 @@
+<?php //Last updated 15-1-17
+$lang = array();
+
+$lang['LANGUAGE'] = 'English';
+$lang['AUTHOR'] = 'ChimpGamer';
+
+$lang['HOME_TITLE'] = 'Welcome back,';
+$lang['HOME_NO_TICKETS'] = "You haven't created any ticket yet. Click <a href='create.php'>here</a> to create one";
+$lang['HOME_TICKET'] = 'Ticket';
+$lang['HOME_LAST_RESPONSE'] = 'Last Response';
+$lang['HOME_STATUS'] = 'Status';
+$lang['HOME_ACTION'] = 'Action';
+
+$lang['MENU_HOME'] = 'Home';
+$lang['MENU_TICKETS'] = 'Tickets';
+$lang['MENU_TICKETS_CREATETICKET'] = 'Create Ticket';
+$lang['MENU_LOGOUT'] = 'Logout';
+
+$lang['TICKET_CREATE_TITLE'] = 'Title';
+$lang['TICKET_CREATE_MESSAGE'] = 'Message';
+$lang['TICKET_CREATE_SUBMIT'] = 'Submit';
+
+$lang['VAR_NO_RESPONSE'] = 'No Response';
+$lang['VAR_OPEN'] = 'OPEN';
+$lang['VAR_CLOSED'] = 'CLOSED';
+$lang['VAR_VIEW'] = 'view';
