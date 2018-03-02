4d8941ae6963f4a818be777dc7be6ba0cb22eee2
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2018-03-02 14:36:50 +0100

    Add files via upload

---
+++ Webbie/languages/Russian.php
@@ -0,0 +1,237 @@
+<?php //Last updated 01/03/2018
+$lang = array();
+
+$lang['LANGUAGE'] = 'Russian';
+$lang['AUTHOR'] = 'Erik Ghazaryan';
+
+$lang['TITLE_OVERVIEW'] = 'Обзор';
+$lang['TITLE_ANALYTICS'] = 'Аналитика';
+
+$lang['TITLE_PLAYERS'] = 'Игроки';
+$lang['TITLE_PUNISHMENTS'] = 'Наказания';
+$lang['TITLE_REPORTS'] = 'Отчеты';
+$lang['TITLE_CHAT'] = 'Чат';
+$lang['TITLE_CHATLOGS'] = 'ЧатЛоги';
+$lang['TITLE_HELPOP'] = 'Помощь';
+$lang['TITLE_TICKETS'] = 'Тикеты';
+
+$lang['TITLE_SETTINGS'] = 'Настройки';
+$lang['TITLE_MOTD'] = 'MOTD';
+$lang['TITLE_PERMISSIONS'] = 'Права';
+$lang['TITLE_TAGS'] = 'Теги';
+$lang['TITLE_FILTER'] = 'Фильтр';
+$lang['TITLE_COMMANDBLOCKER'] = 'Блокировка команд';
+$lang['TITLE_ANNOUNCEMENTS'] = 'Объявления';
+$lang['TITLE_TABCOMPLETECOMMANDS'] = 'TabCompleteCommands';
+
+$lang['TITLE_GROUPSANDACCOUNTS'] = 'Группы и Аккаунты';
+
+$lang['TITLE_TASKS'] = 'Задачи';
+$lang['TITLE_ACCOUNTSETTINGS'] = 'Настройки Аккаунта';
+$lang['TITLE_LOGOUT'] = 'Выйти';
+
+$lang['TITLE_DASHBOARD'] = 'Инструментальная панель';
+$lang['TITLE_NETWORK'] = 'Сеть';
+$lang['TITLE_NETWORKSETTINGS'] = 'Настройки сети';
+$lang['TITLE_ADMINISTRATION'] = 'Администрация';
+$lang['TITLE_ADDONS'] = 'Дополнения';
+$lang['TITLE_ACCOUNT'] = 'Аккаунт';
+
+$lang['TITLE_MOTDANDMAINTENANCE'] = 'MOTD и Maintenance';
+
+$lang['DASHBOARD_TOTALPLAYERS'] = 'Всего Игроков';
+$lang['DASHBOARD_TODAYONLINEPLAYERS'] = 'Сегодня Онлайн Игроков';
+$lang['DASHBOARD_TODAYSNEWPLAYERS'] = 'Сегодня Новых Игроков';
+$lang['DASHBOARD_TODAYSPLAYTIME'] = 'Сегодня сыграно (время)';
+$lang['DASHBOARD_PLAYERSTATISTICS'] = 'Статистика Игроков';
+$lang['DASHBOARD_NEWESTPLAYERS'] = 'Новые Игроки';
+
+$lang['ANALYTICS_ONLINE'] = 'Онлайн';
+$lang['ANALYTICS_AVERAGESTATS'] = 'Средняя Статистика';
+$lang['ANALYTICS_MOSTPLAYEDVERSIONS'] = 'Популярные Версии';
+$lang['ANALYTICS_PLAYERREGIONS'] = 'Регионы Игроков';
+$lang['ANALYTICS_PLAYTIME'] = 'Сыграно (время)';
+$lang['ANALYTICS_USERRETENTION'] = 'Задержка Игроков';
+$lang['ANALYTICS_FUTUREPORJECTIONS'] = 'Будущие Прогнозы';
+
+$lang['PLAYER_PLAYERINFORMATION'] = 'Информация Об Игроке';
+$lang['PLAYER_PLAYERSTATISTICS'] = 'Статистика Игрока';
+$lang['PLAYER_LATESTSESSIONS'] = 'Последние Сеансы';
+$lang['PLAYER_NOTES'] = 'Заметки';
+$lang['PLAYER_PUNISHMENTS'] = 'Наказания';
+$lang['PLAYER_USERNAME'] = 'Имя Игрока';
+$lang['PLAYER_NICKNAME'] = 'Кличка';
+$lang['PLAYER_UUID'] = 'UUID';
+$lang['PLAYER_COUNTRY'] = 'Страна';
+$lang['PLAYER_LATESTMINECRAFTVERSION'] = 'Последняя Версия Minecraft';
+$lang['PLAYER_IPADDRESS'] = 'IP Адрес';
+$lang['PLAYER_JOINED'] = 'Присоединился';
+$lang['PLAYER_LASTLOGIN'] = 'Последний Вход';
+$lang['PLAYER_LASTLOGOUT'] = 'Последний Выход';
+$lang['PLAYER_ONLINE'] = 'Онлайн';
+$lang['PLAYER_PLAYTIME'] = 'Сыграно (время)';
+$lang['PLAYER_AVERAGEPLAYTIME'] = 'В Среднем Сыграно (время)';
+$lang['PLAYER_JOINSAT'] = 'Игрок Обычно Присоединяется К';
+$lang['PLAYER_ALTACCOUNTS'] = 'Дополнительные Аккаунты';
+$lang['PLAYER_SEARCH'] = 'Поиск Игрока По Имени, UUID или IP-адресу';
+
+$lang['PUNISHMENTS_PUNISHMENT'] = 'Наказание';
+$lang['PUNISHMENTS_TYPE'] = 'Тип';
+$lang['PUNISHMENTS_PUNISHED'] = 'Наказан';
+$lang['PUNISHMENTS_PUNISHER'] = 'Каратель';
+$lang['PUNISHMENTS_TIME'] = 'Время';
+$lang['PUNISHMENTS_END'] = 'Конец';
+$lang['PUNISHMENTS_SERVER'] = 'Сервер';
+$lang['PUNISHMENTS_REASON'] = 'Причина';
+$lang['PUNISHMENTS_STATUS'] = 'Статус';
+$lang['PUNISHMENTS_UNBANNER'] = 'Unbanner';
+$lang['PUNISHMENTS_ACTIVE'] = 'Активен';
+$lang['PUNISHMENTS_EXPIRED'] = 'Истек';
+$lang['PUNISHMENTS_SEARCH'] = 'Поиск Наказания По IP, Наказан, Каратель Или По Причине';
+
+$lang['REPORTS_REPORT'] = 'Отчет';
+$lang['REPORTS_REPORTER'] = 'Докладчик';
+$lang['REPORTS_CLOSE'] = 'Закрыть';
+$lang['REPORTS_SEARCH'] = 'Поиск Отчетов По Игроком, Репортером Или По Причиной';
+
+$lang['CHAT_SEARCH'] = 'Поиск Чата По UUID, Сообщению, Серверу Или По IP';
+
+$lang['NOTES_NOTE'] = 'Заметка';
+
+$lang['CHATLOG_CHATLOGID'] = 'ЧатЛог ID';
+$lang['CHATLOG_TRACKED'] = 'Отслеживается';
+$lang['CHATLOG_CHATLOGS'] = 'ЧатЛоги';
+$lang['CHATLOG_SEARCH'] = 'Поиск ЧатЛога По Создателю, UUID Или По Трекингу';
+
+$lang['HELPOP_REQUESTER'] = 'Заявщик';
+$lang['HELPOP_SEARCH'] = 'Поиск Заявок По Запросу, Серверу Или По Сообщению';
+
+$lang['TICKET_TICKET'] = 'Тикет';
+$lang['TICKET_TITLE'] = 'Название';
+$lang['TICKET_ASSIGNEDTO'] = 'Назначено';
+$lang['TICKET_LASTUPDATE'] = 'Последнее обновление';
+$lang['TICKET_ANSWER'] = 'Ответ';
+$lang['TICKET_CLOSE'] = 'Закрыть';
+$lang["TICKET_SEARCH"] = 'Поиск Тикетов По Создателю, Названию Или По Назначению';
+$lang['TICKET_CREATED'] = 'Создан';
+$lang['TICKET_PRIORITY'] = 'Приоритет';
+$lang['TICKET_PRIORITY_LOW'] = 'Низкий';
+$lang['TICKET_PRIORITY_MEDIUM'] = 'Средний';
+$lang['TICKET_PRIORITY_HIGH'] = 'Высокий';
+$lang['TICKET_PRIORITY_NO_PRIORITY'] = 'Нет Приоритета';
+
+$lang['SETTINGS_SEARCH'] = 'Поиск Настройки По Переменной';
+
+$lang['MOTD_MOTDGENERATOR'] = 'Генератор MOTD';
+$lang['MOTD_KICKMSG'] = 'Kick Сообщение';
+$lang['MOTD_PREVIEW'] = 'Просмотр';
+$lang['MOTD_MAINTENANCEMODE'] = 'Режим Обслуживания';
+
+$lang['PERMISSIONS_GROUP_NAME'] = 'Имя';
+$lang['PERMISSIONS_GROUP_LADDER'] = 'Лестница';
+$lang['PERMISSIONS_GROUP_RANK'] = 'Ранг';
+$lang['PERMISSIONS_PERMISSIONS_WORLD'] = 'Мир';
+$lang['PERMISSIONS_EXPIRES'] = 'Истекает';
+$lang['PERMISSIONS_PREFIX'] = 'Префикс';
+$lang['PERMISSIONS_SUFFIX'] = 'Суффикс';
+$lang['PERMISSIONS_WEIGHT'] = 'Вес';
+$lang['PERMISSIONS_ISDEFAULT'] = 'По Умолчанию';
+$lang['PERMISSIONS_PERMISSION'] = 'Права';
+$lang['PERMISSIONS_GROUP_CREATE'] = 'Создать Группу';
+$lang['PERMISSIONS_PERMISSION_ADD'] = 'Добавить Права';
+$lang['PERMISSIONS_GROUP_MEMBER_ADD'] = 'Добавить Участника';
+$lang['PERMISSIONS_GROUP_MEMBER_LIST'] = 'Список Участников';
+$lang['PERMISSIONS_GROUP_SEARCH'] = 'Поиск Группы По ID Или По Имени';
+$lang['PERMISSIONS_PLAYER_SEARCH'] = 'Поиск Игрока По Имени, Префиксу Или По Суффиксу';
+
+$lang['PERMISSIONS_EDITPERMS'] = 'Изменить Права';
+$lang['PERMISSIONS_EDITINHERITANCES'] = 'Изменить Наследование';
+$lang['PERMISSIONS_EDITGROUPSETTINGS'] = 'Изменить Настройки Группы';
+$lang['PERMISSIONS_SELECTGROUP'] = 'Выбрать Группу';
+
+$lang['TAGS_TAG_NAME'] = 'Имя';
+$lang['TAGS_TAG_TAG'] = 'Метка';
+$lang['TAGS_TAG_DESCRIPTION'] = 'Описание';
+$lang['TAGS_TAG_CREATE'] = 'Создать Метку';
+
+$lang['FILTER_ADDWORD'] = 'Добавить Слово';
+
+$lang['COMMANDBLOCKER_ADDCOMMAND'] = 'Добавить Команду';
+
+$lang['ANNOUNCEMENTS_TYPE'] = 'Тип';
+$lang['ANNOUNCEMENTS_MESSAGE'] = 'Сообщение';
+$lang['ANNOUNCEMENTS_CREATE_ANNOUNCEMENT'] = 'Создать Объявление';
+
+$lang['TABCOMPLETECOMMANDS_ADDCOMMAND'] = 'Добавить Команду';
+
+$lang['GROUPANDACCOUNTS_GROUP'] = 'Группа';
+$lang['GROUPANDACCOUNTS_USER'] = 'Пользователь';
+
+$lang['TASKS_YOURTASKS'] = 'Ваши Задачи';
+
+$lang['ACCOUNTSETTINGS_OLDPASS'] = 'Старый Пароль';
+$lang['ACCOUNTSETTINGS_NEWPASS'] = 'Новый Пароль';
+$lang['ACCOUNTSETTINGS_SELECTED_LANG'] = 'Выбранный Язык:';
+
+$lang['LOGIN_TITLE'] = 'Вход В Систему';
+$lang['LOGIN_USERNAME'] = 'Имя Пользователя';
+$lang['LOGIN_PASSWORD'] = 'Пароль';
+$lang['LOGIN_REMEMBERME'] = 'Вспомни Меня';
+$lang['LOGIN_LOGIN'] = 'Вход';
+$lang['LOGIN_WRONGPASSWORD'] = 'Неверный Пароль';
+$lang['LOGIN_LOGGINGIN'] = 'Входим...';
+$lang['LOGIN_NODBCONNECTION'] = 'Не Удалось Подключиться К Базе Данных';
+
+$lang['VAR_ID'] = 'ID';
+$lang['VAR_VIEW'] = 'Вид';
+$lang['VAR_PUNISHMENT'] = 'Наказание';
+$lang['VAR_PLAYER'] = 'Игрок';
+$lang['VAR_PUNISHER'] = 'Каратель';
+$lang['VAR_REASON'] = 'Причина';
+$lang['VAR_TIME'] = 'Время';
+$lang['VAR_STARTED'] = 'Начатый';
+$lang['VAR_ENDED'] = 'Закончившийся';
+$lang['VAR_VERSION'] = 'Версия';
+$lang['VAR_UNBAN'] = 'Разбанить';
+$lang['VAR_DELETEBAN'] = 'УДАЛИТЬ ЗАПРЕТ';
+$lang['VAR_UNMUTE'] = 'ВКЛЮЧИТЬ ЧАТ';
+$lang['VAR_DELETEMUTE'] = 'УДАЛИТЬ MUTE ЧATA';
+$lang['VAR_DELETEKICK'] = 'УДАЛИТЬ КИК';
+$lang['VAR_UNWARN'] = 'СНЯТЬ ПРЕДУПРЕЖДЕНИЕ';
+$lang['VAR_DELETEWARN'] = 'УДАЛИТЬ ПРЕДУПРЕЖДЕНИЕ';
+$lang['VAR_MESSAGE'] = 'Сообщение';
+$lang['VAR_SERVER'] = 'Сервер';
+$lang['VAR_TICKET'] = 'Тикет';
+$lang['VAR_CREATOR'] = 'Создатель';
+$lang['VAR_ON'] = 'Включено';
+$lang['VAR_OFF'] = 'Выключен';
+$lang['VAR_ICON'] = 'Иконка';
+$lang['VAR_DELETE'] = 'УДАЛИТЬ';
+$lang['VAR_DEFAULT'] = 'По Умолчанию';
+$lang['VAR_ACTION'] = 'Действие';
+$lang['VAR_PASSWORD'] = 'Пароль';
+$lang['VAR_LANGUAGES'] = 'Языки';
+$lang['VAR_GLOBAL'] = 'Глобальный';
+$lang['VAR_SAVE'] = 'Сохранить';
+$lang['VAR_CANCEL'] = 'Отмена';
+$lang['VAR_MONTHLY'] = 'Ежемесячно';
+$lang['VAR_WEEKLY'] = 'Еженедельно';
+$lang['VAR_DAILY'] = 'Ежедневно';
+$lang['VAR_TODAY'] = 'Cегодня';
+$lang['VAR_THISWEEK'] = 'На Этой Неделе';
+$lang['VAR_TOTAL'] = 'Всего';
+$lang['VAR_LOADING'] = 'Загрузка...';
+$lang['VAR_BY'] = 'От';
+$lang['VAR_ALL'] = 'ВСЕ';
+$lang['VAR_UNKNOWN'] = 'Неизвестный';
+$lang['VAR_BACK'] = 'Назад';
+$lang['VAR_ACTIVE'] = 'Активно';
+$lang['VAR_INACTIVE'] = 'Неактивен';
+$lang['VAR_TRUE'] = 'Верно';
+$lang['VAR_FALSE'] = 'Неверно';
+$lang['VAR_NEVER'] = 'Никогда';
+
+$lang['TEXT_NORESULTS'] = 'Результаты Не Могут Быть Отображены Или Отсутствуют';
+$lang['TEXT_NORESULTS_SEARCH'] = 'Результаты Не Найдены';
+$lang['TEXT_SELECTOPTION'] = ' -- выберите параметр -- ';
+$lang['TEXT_NOTASKS'] = 'Нет Задач! Хорошего Дня :)';
