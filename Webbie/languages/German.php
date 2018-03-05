ee82098509d7ddba68a88a396be4b61e4092f64d
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2018-03-05 17:17:04 +0100

    Add files via upload

---
+++ Webbie/languages/German.php
@@ -0,0 +1,233 @@
+<?php //Last updated 05.03.2018
+$lang = array();
+
+$lang['LANGUAGE'] = 'Deutsch';
+$lang['AUTHOR'] = 'xanily';
+
+$lang['TITLE_OVERVIEW'] = 'Übersicht';
+$lang['TITLE_ANALYTICS'] = 'Statistiken';
+
+$lang['TITLE_PLAYERS'] = 'Spieler';
+$lang['TITLE_PUNISHMENTS'] = 'Bestrafungen';
+$lang['TITLE_REPORTS'] = 'Reports';
+$lang['TITLE_CHAT'] = 'Chat';
+$lang['TITLE_CHATLOGS'] = 'ChatLogs';
+$lang['TITLE_HELPOP'] = 'Helpop';
+$lang['TITLE_TICKETS'] = 'Tickets';
+
+$lang['TITLE_SETTINGS'] = 'Einstellungen';
+$lang['TITLE_MOTD'] = 'MOTD';
+$lang['TITLE_PERMISSIONS'] = 'Berechtigungen';
+$lang['TITLE_TAGS'] = 'Tags';
+$lang['TITLE_FILTER'] = 'Filter';
+$lang['TITLE_COMMANDBLOCKER'] = 'Befehlsblocker';
+$lang['TITLE_ANNOUNCEMENTS'] = 'Ankündigungen';
+$lang['TITLE_TABCOMPLETECOMMANDS'] = 'Tab Complete Befehle';
+
+$lang['TITLE_GROUPSANDACCOUNTS'] = 'Gruppen & Accounts';
+
+$lang['TITLE_TASKS'] = 'Aufgaben';
+$lang['TITLE_ACCOUNTSETTINGS'] = 'Account Einstellungen';
+$lang['TITLE_LOGOUT'] = 'Ausloggen';
+
+$lang['TITLE_DASHBOARD'] = 'Übersichtsseite';
+$lang['TITLE_NETWORK'] = 'Netzwerk';
+$lang['TITLE_NETWORKSETTINGS'] = 'Netzwerk Eisntellungen';
+$lang['TITLE_ADMINISTRATION'] = 'Administration';
+$lang['TITLE_ADDONS'] = 'Addons';
+$lang['TITLE_ACCOUNT'] = 'Account';
+
+$lang['TITLE_MOTDANDMAINTENANCE'] = 'MOTD & Wartung';
+
+$lang['DASHBOARD_TOTALPLAYERS'] = 'Gesamtanzahl der Spieler';
+$lang['DASHBOARD_TODAYONLINEPLAYERS'] = 'Heutige Online Spieler';
+$lang['DASHBOARD_TODAYSNEWPLAYERS'] = 'Neue Spieler von heute';
+$lang['DASHBOARD_TODAYSPLAYTIME'] = 'Heutige Spielzeit';
+$lang['DASHBOARD_PLAYERSTATISTICS'] = 'Spieler Statistiken';
+$lang['DASHBOARD_NEWESTPLAYERS'] = 'Neuste Spieler';
+
+$lang['ANALYTICS_ONLINE'] = 'Online';
+$lang['ANALYTICS_AVERAGESTATS'] = 'Durchschnittliche Statistik';
+$lang['ANALYTICS_MOSTPLAYEDVERSIONS'] = 'Meist gespielte Versionen';
+$lang['ANALYTICS_PLAYERREGIONS'] = 'Spieler Regionen';
+$lang['ANALYTICS_PLAYTIME'] = 'Spielzeit';
+$lang['ANALYTICS_USERRETENTION'] = 'Nutzerbindung';
+$lang['ANALYTICS_FUTUREPORJECTIONS'] = 'Zukunftsprognosen';
+
+$lang['PLAYER_PLAYERINFORMATION'] = 'Spieler Informationen';
+$lang['PLAYER_PLAYERSTATISTICS'] = 'Spieler Statistiken';
+$lang['PLAYER_LATESTSESSIONS'] = 'Letzte Sessions';
+$lang['PLAYER_NOTES'] = 'Notizen';
+$lang['PLAYER_PUNISHMENTS'] = 'Bestrafungen';
+$lang['PLAYER_USERNAME'] = 'Benutzername';
+$lang['PLAYER_NICKNAME'] = 'Spitzname';
+$lang['PLAYER_UUID'] = 'UUID';
+$lang['PLAYER_COUNTRY'] = 'Land';
+$lang['PLAYER_LATESTMINECRAFTVERSION'] = 'Neueste Minecraft Version';
+$lang['PLAYER_IPADDRESS'] = 'IP Addresse';
+$lang['PLAYER_JOINED'] = 'Beigetreten';
+$lang['PLAYER_LASTLOGIN'] = 'Letzter Login';
+$lang['PLAYER_LASTLOGOUT'] = 'Letzter Logout';
+$lang['PLAYER_ONLINE'] = 'Online';
+$lang['PLAYER_PLAYTIME'] = 'Spielzeit';
+$lang['PLAYER_AVERAGEPLAYTIME'] = 'Durchschnittliche Spielzeit';
+$lang['PLAYER_JOINSAT'] = 'Üblicherweise verbindet sich der Spieler um';
+$lang['PLAYER_ALTACCOUNTS'] = 'Zusätzliche Accounts';
+$lang['PLAYER_SEARCH'] = 'Spieler nach Name, UUID oder IP-Adresse suchen';
+
+$lang['PUNISHMENTS_PUNISHMENT'] = 'Bestrafung';
+$lang['PUNISHMENTS_TYPE'] = 'Typ';
+$lang['PUNISHMENTS_PUNISHED'] = 'Bestraft';
+$lang['PUNISHMENTS_PUNISHER'] = 'Bestrafer';
+$lang['PUNISHMENTS_TIME'] = 'Zeit';
+$lang['PUNISHMENTS_END'] = 'Ende';
+$lang['PUNISHMENTS_SERVER'] = 'Server';
+$lang['PUNISHMENTS_REASON'] = 'Begründung';
+$lang['PUNISHMENTS_STATUS'] = 'Status';
+$lang['PUNISHMENTS_UNBANNER'] = 'Entbanner';
+$lang['PUNISHMENTS_ACTIVE'] = 'Aktiv';
+$lang['PUNISHMENTS_EXPIRED'] = 'Verfallen';
+$lang['PUNISHMENTS_SEARCH'] = 'Suche Bestrafung nach IP, Bestrafung, Bestrafung, Bestrafung oder Grund';
+
+$lang['REPORTS_REPORT'] = 'Bericht';
+$lang['REPORTS_REPORTER'] = 'Verfasser';
+$lang['REPORTS_CLOSE'] = 'Schließen';
+$lang['REPORTS_SEARCH'] = 'Berichte nach Spieler, Reporter oder Grund suchen';
+
+$lang['CHAT_SEARCH'] = 'Chat nach uuid, Nachricht, Server oder IP suchen';
+
+$lang['NOTES_NOTE'] = 'Notiz';
+
+$lang['CHATLOG_CHATLOGID'] = 'Chatlog ID';
+$lang['CHATLOG_TRACKED'] = 'Verfolgt';
+$lang['CHATLOG_CHATLOGS'] = 'Chatlogs';
+$lang['CHATLOG_SEARCH'] = 'Suche Chatlog nach Ersteller, uuid oder verfolgt';
+
+$lang['HELPOP_REQUESTER'] = 'Antragsteller';
+$lang['HELPOP_SEARCH'] = 'Suchhilfeanfragen nach Antragsteller, Server oder Nachricht suchen';
+
+$lang['TICKET_TICKET'] = 'Ticket';
+$lang['TICKET_TITLE'] = 'Titel';
+$lang['TICKET_ASSIGNEDTO'] = 'Zugewiesen an';
+$lang['TICKET_LASTUPDATE'] = 'Letztes Update';
+$lang['TICKET_ANSWER'] = 'Antwort';
+$lang['TICKET_CLOSE'] = 'Schließen';
+$lang["TICKET_SEARCH"] = 'Suche Tickets nach Ersteller, Titel oder Zugewiesen an';
+$lang['TICKET_CREATED'] = 'Erstellt';
+$lang['TICKET_PRIORITY'] = 'Priorität';
+$lang['TICKET_PRIORITY_LOW'] = 'Niedrig';
+$lang['TICKET_PRIORITY_MEDIUM'] = 'Normal';
+$lang['TICKET_PRIORITY_HIGH'] = 'Hoch';
+$lang['TICKET_PRIORITY_NO_PRIORITY'] = 'Keine Priorität';
+
+$lang['SETTINGS_SEARCH'] = 'Suche nach Einstellung über Variable';
+
+$lang['MOTD_MOTDGENERATOR'] = 'MOTD Generator';
+$lang['MOTD_KICKMSG'] = 'Kick Nachricht';
+$lang['MOTD_PREVIEW'] = 'Vorschau';
+$lang['MOTD_MAINTENANCEMODE'] = 'Wartungsmodus';
+
+$lang['PERMISSIONS_GROUP_NAME'] = 'Name';
+$lang['PERMISSIONS_GROUP_LADDER'] = 'Aufstiegsleiter';
+$lang['PERMISSIONS_GROUP_RANK'] = 'Rang';
+$lang['PERMISSIONS_PERMISSIONS_WORLD'] = 'Welt';
+$lang['PERMISSIONS_EXPIRES'] = 'Verfällt';
+$lang['PERMISSIONS_PREFIX'] = 'Prefix';
+$lang['PERMISSIONS_SUFFIX'] = 'Suffix';
+$lang['PERMISSIONS_ISDEFAULT'] = 'ist Standart';
+$lang['PERMISSIONS_PERMISSION'] = 'Berechtigung';
+$lang['PERMISSIONS_GROUP_CREATE'] = 'Gruppe erstellen';
+$lang['PERMISSIONS_GROUP_PREFIX_ADD'] = 'Prefix hinzufügen';
+$lang['PERMISSIONS_GROUP_SUFFIX_ADD'] = 'Suffix hinzufügen';
+$lang['PERMISSIONS_PERMISSION_ADD'] = 'Berechtigung hinzufügen';
+$lang['PERMISSIONS_GROUP_MEMBER_ADD'] = 'Mitglied hinzufügen';
+$lang['PERMISSIONS_GROUP_MEMBER_LIST'] = 'Mitglieder auflisten';
+$lang['PERMISSIONS_GROUP_SEARCH'] = 'Gruppe nach ID oder Name durchsuchen';
+$lang['PERMISSIONS_PLAYER_SEARCH'] = 'Spieler nach Name, Präfix oder Suffix suchen';
+
+$lang['TAGS_TAG_NAME'] = 'Name';
+$lang['TAGS_TAG_TAG'] = 'Tag';
+$lang['TAGS_TAG_DESCRIPTION'] = 'Beschreibung';
+$lang['TAGS_TAG_CREATE'] = 'Erstelle Tag';
+
+$lang['FILTER_ADDWORD'] = 'Wort hinzufügen';
+
+$lang['COMMANDBLOCKER_ADDCOMMAND'] = 'Befehl hinzufügen';
+
+$lang['ANNOUNCEMENTS_TYPE'] = 'Typ';
+$lang['ANNOUNCEMENTS_MESSAGE'] = 'Nachricht';
+$lang['ANNOUNCEMENTS_CREATE_ANNOUNCEMENT'] = 'Ankündigung erstellen';
+
+$lang['TABCOMPLETECOMMANDS_ADDCOMMAND'] = 'Befehl hinzufügen';
+
+$lang['GROUPANDACCOUNTS_GROUP'] = 'Gruppe';
+$lang['GROUPANDACCOUNTS_USER'] = 'Benutzer';
+
+$lang['TASKS_YOURTASKS'] = 'Deine Aufgaben';
+
+$lang['ACCOUNTSETTINGS_OLDPASS'] = 'Altes Passwort';
+$lang['ACCOUNTSETTINGS_NEWPASS'] = 'Neues Passwort';
+$lang['ACCOUNTSETTINGS_SELECTED_LANG'] = 'Ausgewählte Sprache:';
+
+$lang['LOGIN_TITLE'] = 'NetworkManager';
+$lang['LOGIN_USERNAME'] = 'Benutzername';
+$lang['LOGIN_PASSWORD'] = 'Passwort';
+$lang['LOGIN_REMEMBERME'] = 'Eingeloggt bleiben';
+$lang['LOGIN_LOGIN'] = 'Einloggen';
+$lang['LOGIN_WRONGPASSWORD'] = 'Falsches Passwort';
+$lang['LOGIN_LOGGINGIN'] = 'Logge ein...';
+$lang['LOGIN_NODBCONNECTION'] = 'Konnte keine Verbindung zur Datenbank herstellen';
+
+$lang['VAR_ID'] = 'ID';
+$lang['VAR_VIEW'] = 'Ansicht';
+$lang['VAR_PUNISHMENT'] = 'Bestrafung';
+$lang['VAR_PLAYER'] = 'Spieler';
+$lang['VAR_PUNISHER'] = 'Bestrafer';
+$lang['VAR_REASON'] = 'Grund';
+$lang['VAR_TIME'] = 'Zeit';
+$lang['VAR_STARTED'] = 'Gestartet';
+$lang['VAR_ENDED'] = 'Beendet';
+$lang['VAR_VERSION'] = 'Version';
+$lang['VAR_UNBAN'] = 'Entbannen';
+$lang['VAR_DELETEBAN'] = 'Ban löschen';
+$lang['VAR_UNMUTE'] = 'Stummschaltung aufheben';
+$lang['VAR_DELETEMUTE'] = 'Stummschaltung löschen';
+$lang['VAR_DELETEKICK'] = 'Kick löschen';
+$lang['VAR_UNWARN'] = 'Entwarnen';
+$lang['VAR_DELETEWARN'] = 'Warnung löschen';
+$lang['VAR_MESSAGE'] = 'Nachricht';
+$lang['VAR_SERVER'] = 'Server';
+$lang['VAR_TICKET'] = 'Ticket';
+$lang['VAR_CREATOR'] = 'Ersteller';
+$lang['VAR_ON'] = 'An';
+$lang['VAR_OFF'] = 'Aus';
+$lang['VAR_ICON'] = 'Icon';
+$lang['VAR_DELETE'] = 'Löschen';
+$lang['VAR_DEFAULT'] = 'Voreinstellung';
+$lang['VAR_ACTION'] = 'Aktion';
+$lang['VAR_PASSWORD'] = 'Passwort';
+$lang['VAR_LANGUAGES'] = 'Sprachen';
+$lang['VAR_GLOBAL'] = 'Global';
+$lang['VAR_SAVE'] = 'Speichern';
+$lang['VAR_CANCEL'] = 'Abbrchen';
+$lang['VAR_MONTHLY'] = 'Monatlich';
+$lang['VAR_WEEKLY'] = 'Wöchentlich';
+$lang['VAR_DAILY'] = 'Täglich';
+$lang['VAR_TODAY'] = 'Heute';
+$lang['VAR_THISWEEK'] = 'Diese Woche';
+$lang['VAR_TOTAL'] = 'Total';
+$lang['VAR_LOADING'] = 'Lade...';
+$lang['VAR_BY'] = 'von';
+$lang['VAR_ALL'] = 'alles';
+$lang['VAR_UNKNOWN'] = 'Unbekannt';
+$lang['VAR_BACK'] = 'Zurück';
+$lang['VAR_ACTIVE'] = 'Aktiv';
+$lang['VAR_INACTIVE'] = 'Inaktiv';
+$lang['VAR_TRUE'] = 'Wahr';
+$lang['VAR_FALSE'] = 'Falsch';
+$lang['VAR_NEVER'] = 'Nie';
+
+$lang['TEXT_NORESULTS'] = 'Es konnten keine Ergebnisse angezeigt werden.';
+$lang['TEXT_NORESULTS_SEARCH'] = 'Keine Ergebnisse gefunden für';
+$lang['TEXT_SELECTOPTION'] = ' -- Option auswählen -- ';
+$lang['TEXT_NOTASKS'] = 'Keine Aufgaben! Einen schönen Tag noch :)';
