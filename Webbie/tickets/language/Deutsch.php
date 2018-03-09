c5b96b653483a627e40ee643aacfad0f8b226e94
xanily <xanily@faldoria.de>
2018-03-09 18:22:03 +0100

    Create german translation for tickets

---
+++ Webbie/tickets/language/Deutsch.php
@@ -0,0 +1,26 @@
+<?php //Last updated 09-03-18
+$lang = array();
+
+$lang['LANGUAGE'] = 'Deutsch';
+$lang['AUTHOR'] = 'xanily';
+
+$lang['HOME_TITLE'] = 'Willkommen zurück,';
+$lang['HOME_NO_TICKETS'] = "Du hast noch kein Ticket erstellt. Klick <a href='create.php'>hier</a> um ein Ticket zu erstellen.";
+$lang['HOME_TICKET'] = 'Ticket';
+$lang['HOME_LAST_RESPONSE'] = 'Letzte Antwort';
+$lang['HOME_STATUS'] = 'Status';
+$lang['HOME_ACTION'] = 'Aktion';
+
+$lang['MENU_HOME'] = 'Start';
+$lang['MENU_TICKETS'] = 'Tickets';
+$lang['MENU_TICKETS_CREATETICKET'] = 'Ticket erstellen';
+$lang['MENU_LOGOUT'] = 'Ausloggen';
+
+$lang['TICKET_CREATE_TITLE'] = 'Titel';
+$lang['TICKET_CREATE_MESSAGE'] = 'Nachricht';
+$lang['TICKET_CREATE_SUBMIT'] = 'Ticket erstellen';
+
+$lang['VAR_NO_RESPONSE'] = 'Keine Antwort';
+$lang['VAR_OPEN'] = 'Offen';
+$lang['VAR_CLOSED'] = 'Geschlossen';
+$lang['VAR_VIEW'] = 'view';
