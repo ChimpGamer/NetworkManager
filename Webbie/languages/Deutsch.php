4f65e0b2aeab25adf2c734391c1b516359c8433d
xanily <xanily@faldoria.de>
2018-03-10 15:14:25 +0100

    Update german language file / adding new translations Also update vowel mutations with html code

--- Webbie/languages/Deutsch.php
+++ Webbie/languages/Deutsch.php
@@ -1,10 +1,10 @@
-<?php //Last updated 05.03.2018
+<?php //Last updated 10.03.2018
 $lang = array();
 
 $lang['LANGUAGE'] = 'Deutsch';
 $lang['AUTHOR'] = 'xanily';
 
-$lang['TITLE_OVERVIEW'] = 'Übersicht';
+$lang['TITLE_OVERVIEW'] = '&Uuml;bersicht';
 $lang['TITLE_ANALYTICS'] = 'Statistiken';
 
 $lang['TITLE_PLAYERS'] = 'Spieler';
@@ -18,10 +18,11 @@ $lang['TITLE_TICKETS'] = 'Tickets';
 $lang['TITLE_SETTINGS'] = 'Einstellungen';
 $lang['TITLE_MOTD'] = 'MOTD';
 $lang['TITLE_PERMISSIONS'] = 'Berechtigungen';
+$lang['TITLE_SERVERS'] = 'Servers';
 $lang['TITLE_TAGS'] = 'Tags';
 $lang['TITLE_FILTER'] = 'Filter';
 $lang['TITLE_COMMANDBLOCKER'] = 'Befehlsblocker';
-$lang['TITLE_ANNOUNCEMENTS'] = 'Ankündigungen';
+$lang['TITLE_ANNOUNCEMENTS'] = 'Ank&uuml;ndigungen';
 $lang['TITLE_TABCOMPLETECOMMANDS'] = 'Tab Complete Befehle';
 
 $lang['TITLE_GROUPSANDACCOUNTS'] = 'Gruppen & Accounts';
@@ -30,7 +31,7 @@ $lang['TITLE_TASKS'] = 'Aufgaben';
 $lang['TITLE_ACCOUNTSETTINGS'] = 'Account Einstellungen';
 $lang['TITLE_LOGOUT'] = 'Ausloggen';
 
-$lang['TITLE_DASHBOARD'] = 'Übersichtsseite';
+$lang['TITLE_DASHBOARD'] = '&Uuml;bersichtsseite';
 $lang['TITLE_NETWORK'] = 'Netzwerk';
 $lang['TITLE_NETWORKSETTINGS'] = 'Netzwerk Eisntellungen';
 $lang['TITLE_ADMINISTRATION'] = 'Administration';
@@ -45,6 +46,8 @@ $lang['DASHBOARD_TODAYSNEWPLAYERS'] = 'Neue Spieler von heute';
 $lang['DASHBOARD_TODAYSPLAYTIME'] = 'Heutige Spielzeit';
 $lang['DASHBOARD_PLAYERSTATISTICS'] = 'Spieler Statistiken';
 $lang['DASHBOARD_NEWESTPLAYERS'] = 'Neuste Spieler';
+$lang['DASHBOARD_PLAYERSTATISTICS_TOOLTIP'] = 'Zeigt n&uuml;tzliche Statistiken der Spieleranzahl der letzten 60 Tage an.';
+$lang['DASHBOARD_NEWESTPLAYERS_TOOLTOP'] = 'Zeigt eine Karte mit den Standorten der Spieler, die in den letzten 60 Tagen beigetreten sind.';
 
 $lang['ANALYTICS_ONLINE'] = 'Online';
 $lang['ANALYTICS_AVERAGESTATS'] = 'Durchschnittliche Statistik';
@@ -53,6 +56,13 @@ $lang['ANALYTICS_PLAYERREGIONS'] = 'Spieler Regionen';
 $lang['ANALYTICS_PLAYTIME'] = 'Spielzeit';
 $lang['ANALYTICS_USERRETENTION'] = 'Nutzerbindung';
 $lang['ANALYTICS_FUTUREPORJECTIONS'] = 'Zukunftsprognosen';
+$lang['ANALYTICS_ONLINE_TOOLTOP'] = 'Grafische Darstellung der Online-Spieler &uuml;ber den gesamten Zeitraum.';
+$lang['ANALYTICS_AVERAGESTATS_TOOLTOP'] = 'ANPPD (Average New Players Per Day(Durchschnittliche Anzahl neuer Spieler pro Tag)) und ARPPD (Average Returning Player Per Day(Durchschnittlich wiederkehrender Spieler pro Tag)) zeigen Statistiken auf monatlicher, w&ouml;chentlicher und t&auml;glicher Basis.';
+$lang['ANALYTICS_MOSTPLAYEDVERSIONS_TOOLTIP'] = 'Zeigt die meistgespielten Minecraft-Versionen deiner Spieler an, die als Betrag und Prozentsatz angezeigt werden.';
+$lang['ANALYTICS_PLAYERREGIONS_TOOLTIP'] = 'Zeigt in einem Balkendiagramm und einer Karte an, aus welchem Land deine Spieler kommen.';
+$lang['ANALYTICS_PLAYTIME_TOOLTIP'] = 'Zeigt den Gesamtbetrag der Spielzeit in deinem Netzwerk heute, diese Woche und &uuml;ber den gesamten Zeitraum an.';
+$lang['ANALYTICS_USERRETENTION_TOOLTIP'] = 'Welcher Prozentsatz der Spieler kehrt nach dem ersten Beitritt zur&uuml;ck. Angezeigt in Tagen';
+$lang['ANALYTICS_FUTUREPORJECTIONS_TOOLTIP'] = 'Dieser Algorithmus zeigt dir m&ouml;gliche zuk&uuml;nftige Projektionen von neuen Spielern und aktiven Spielern.';
 
 $lang['PLAYER_PLAYERINFORMATION'] = 'Spieler Informationen';
 $lang['PLAYER_PLAYERSTATISTICS'] = 'Spieler Statistiken';
@@ -71,8 +81,8 @@ $lang['PLAYER_LASTLOGOUT'] = 'Letzter Logout';
 $lang['PLAYER_ONLINE'] = 'Online';
 $lang['PLAYER_PLAYTIME'] = 'Spielzeit';
 $lang['PLAYER_AVERAGEPLAYTIME'] = 'Durchschnittliche Spielzeit';
-$lang['PLAYER_JOINSAT'] = 'Üblicherweise verbindet sich der Spieler um';
-$lang['PLAYER_ALTACCOUNTS'] = 'Zusätzliche Accounts';
+$lang['PLAYER_JOINSAT'] = '&Uuml;blicherweise verbindet sich der Spieler um';
+$lang['PLAYER_ALTACCOUNTS'] = 'Zus&auml;tzliche Accounts';
 $lang['PLAYER_SEARCH'] = 'Spieler nach Name, UUID oder IP-Adresse suchen';
 
 $lang['PUNISHMENTS_PUNISHMENT'] = 'Bestrafung';
@@ -82,26 +92,26 @@ $lang['PUNISHMENTS_PUNISHER'] = 'Bestrafer';
 $lang['PUNISHMENTS_TIME'] = 'Zeit';
 $lang['PUNISHMENTS_END'] = 'Ende';
 $lang['PUNISHMENTS_SERVER'] = 'Server';
-$lang['PUNISHMENTS_REASON'] = 'Begründung';
+$lang['PUNISHMENTS_REASON'] = 'Begr&uuml;ndung';
 $lang['PUNISHMENTS_STATUS'] = 'Status';
 $lang['PUNISHMENTS_UNBANNER'] = 'Entbanner';
 $lang['PUNISHMENTS_ACTIVE'] = 'Aktiv';
 $lang['PUNISHMENTS_EXPIRED'] = 'Verfallen';
-$lang['PUNISHMENTS_SEARCH'] = 'Suche Bestrafung nach IP, Bestrafung, Bestrafung, Bestrafung oder Grund';
+$lang['PUNISHMENTS_SEARCH'] = 'Suche Bestrafung nach Spieler, Bestrafer, IP oder Grund';
 
 $lang['REPORTS_REPORT'] = 'Bericht';
 $lang['REPORTS_REPORTER'] = 'Verfasser';
-$lang['REPORTS_CLOSE'] = 'Schließen';
+$lang['REPORTS_CLOSE'] = 'Schlie&szlig;en';
 $lang['REPORTS_SEARCH'] = 'Berichte nach Spieler, Reporter oder Grund suchen';
 
-$lang['CHAT_SEARCH'] = 'Chat nach uuid, Nachricht, Server oder IP suchen';
+$lang['CHAT_SEARCH'] = 'Chat nach Benutzername, UUID, Nachricht, Server oder IP suchen';
 
 $lang['NOTES_NOTE'] = 'Notiz';
 
 $lang['CHATLOG_CHATLOGID'] = 'Chatlog ID';
 $lang['CHATLOG_TRACKED'] = 'Verfolgt';
 $lang['CHATLOG_CHATLOGS'] = 'Chatlogs';
-$lang['CHATLOG_SEARCH'] = 'Suche Chatlog nach Ersteller, uuid oder verfolgt';
+$lang['CHATLOG_SEARCH'] = 'Suche Chatlog nach Ersteller, UUID oder verfolgt';
 
 $lang['HELPOP_REQUESTER'] = 'Antragsteller';
 $lang['HELPOP_SEARCH'] = 'Suchhilfeanfragen nach Antragsteller, Server oder Nachricht suchen';
@@ -111,16 +121,16 @@ $lang['TICKET_TITLE'] = 'Titel';
 $lang['TICKET_ASSIGNEDTO'] = 'Zugewiesen an';
 $lang['TICKET_LASTUPDATE'] = 'Letztes Update';
 $lang['TICKET_ANSWER'] = 'Antwort';
-$lang['TICKET_CLOSE'] = 'Schließen';
+$lang['TICKET_CLOSE'] = 'Schlie&szlig;en';
 $lang["TICKET_SEARCH"] = 'Suche Tickets nach Ersteller, Titel oder Zugewiesen an';
 $lang['TICKET_CREATED'] = 'Erstellt';
-$lang['TICKET_PRIORITY'] = 'Priorität';
+$lang['TICKET_PRIORITY'] = 'Priorit&auml;t';
 $lang['TICKET_PRIORITY_LOW'] = 'Niedrig';
 $lang['TICKET_PRIORITY_MEDIUM'] = 'Normal';
 $lang['TICKET_PRIORITY_HIGH'] = 'Hoch';
-$lang['TICKET_PRIORITY_NO_PRIORITY'] = 'Keine Priorität';
+$lang['TICKET_PRIORITY_NO_PRIORITY'] = 'Keine Priorit&auml;t';
 
-$lang['SETTINGS_SEARCH'] = 'Suche nach Einstellung über Variable';
+$lang['SETTINGS_SEARCH'] = 'Suche nach Einstellung &uuml;ber Variable';
 
 $lang['MOTD_MOTDGENERATOR'] = 'MOTD Generator';
 $lang['MOTD_KICKMSG'] = 'Kick Nachricht';
@@ -131,34 +141,40 @@ $lang['PERMISSIONS_GROUP_NAME'] = 'Name';
 $lang['PERMISSIONS_GROUP_LADDER'] = 'Aufstiegsleiter';
 $lang['PERMISSIONS_GROUP_RANK'] = 'Rang';
 $lang['PERMISSIONS_PERMISSIONS_WORLD'] = 'Welt';
-$lang['PERMISSIONS_EXPIRES'] = 'Verfällt';
+$lang['PERMISSIONS_EXPIRES'] = 'Verf&auml;llt';
 $lang['PERMISSIONS_PREFIX'] = 'Prefix';
 $lang['PERMISSIONS_SUFFIX'] = 'Suffix';
 $lang['PERMISSIONS_ISDEFAULT'] = 'ist Standart';
 $lang['PERMISSIONS_PERMISSION'] = 'Berechtigung';
 $lang['PERMISSIONS_GROUP_CREATE'] = 'Gruppe erstellen';
-$lang['PERMISSIONS_GROUP_PREFIX_ADD'] = 'Prefix hinzufügen';
-$lang['PERMISSIONS_GROUP_SUFFIX_ADD'] = 'Suffix hinzufügen';
-$lang['PERMISSIONS_PERMISSION_ADD'] = 'Berechtigung hinzufügen';
-$lang['PERMISSIONS_GROUP_MEMBER_ADD'] = 'Mitglied hinzufügen';
+$lang['PERMISSIONS_GROUP_PREFIX_ADD'] = 'Prefix hinzuf&uuml;gen';
+$lang['PERMISSIONS_GROUP_SUFFIX_ADD'] = 'Suffix hinzuf&uuml;gen';
+$lang['PERMISSIONS_PERMISSION_ADD'] = 'Berechtigung hinzuf&uuml;gen';
+$lang['PERMISSIONS_GROUP_MEMBER_ADD'] = 'Mitglied hinzuf&uuml;gen';
 $lang['PERMISSIONS_GROUP_MEMBER_LIST'] = 'Mitglieder auflisten';
 $lang['PERMISSIONS_GROUP_SEARCH'] = 'Gruppe nach ID oder Name durchsuchen';
-$lang['PERMISSIONS_PLAYER_SEARCH'] = 'Spieler nach Name, Präfix oder Suffix suchen';
+$lang['PERMISSIONS_PLAYER_SEARCH'] = 'Spieler nach Name, Pr&auml;fix oder Suffix suchen';
+
+$lang['SERVERS_PORT'] = 'Port';
+$lang['SERVERS_MOTD'] = 'Motd';
+$lang['SERVERS_RESTRICTED'] = 'Eingeschr&auml;nkt';
+$lang['SERVERS_ISLOBBY'] = 'isLobby';
+$lang['SERVERS_ADD_SERVER'] = 'Server hinzuf&uuml;gen';
 
 $lang['TAGS_TAG_NAME'] = 'Name';
 $lang['TAGS_TAG_TAG'] = 'Tag';
 $lang['TAGS_TAG_DESCRIPTION'] = 'Beschreibung';
 $lang['TAGS_TAG_CREATE'] = 'Erstelle Tag';
 
-$lang['FILTER_ADDWORD'] = 'Wort hinzufügen';
+$lang['FILTER_ADDWORD'] = 'Wort hinzuf&uuml;gen';
 
-$lang['COMMANDBLOCKER_ADDCOMMAND'] = 'Befehl hinzufügen';
+$lang['COMMANDBLOCKER_ADDCOMMAND'] = 'Befehl hinzuf&uuml;gen';
 
 $lang['ANNOUNCEMENTS_TYPE'] = 'Typ';
 $lang['ANNOUNCEMENTS_MESSAGE'] = 'Nachricht';
-$lang['ANNOUNCEMENTS_CREATE_ANNOUNCEMENT'] = 'Ankündigung erstellen';
+$lang['ANNOUNCEMENTS_CREATE_ANNOUNCEMENT'] = 'Ank&uuml;ndigung erstellen';
 
-$lang['TABCOMPLETECOMMANDS_ADDCOMMAND'] = 'Befehl hinzufügen';
+$lang['TABCOMPLETECOMMANDS_ADDCOMMAND'] = 'Befehl hinzuf&uuml;gen';
 
 $lang['GROUPANDACCOUNTS_GROUP'] = 'Gruppe';
 $lang['GROUPANDACCOUNTS_USER'] = 'Benutzer';
@@ -167,7 +183,7 @@ $lang['TASKS_YOURTASKS'] = 'Deine Aufgaben';
 
 $lang['ACCOUNTSETTINGS_OLDPASS'] = 'Altes Passwort';
 $lang['ACCOUNTSETTINGS_NEWPASS'] = 'Neues Passwort';
-$lang['ACCOUNTSETTINGS_SELECTED_LANG'] = 'Ausgewählte Sprache:';
+$lang['ACCOUNTSETTINGS_SELECTED_LANG'] = 'Ausgew&auml;hlte Sprache:';
 
 $lang['LOGIN_TITLE'] = 'NetworkManager';
 $lang['LOGIN_USERNAME'] = 'Benutzername';
@@ -189,12 +205,12 @@ $lang['VAR_STARTED'] = 'Gestartet';
 $lang['VAR_ENDED'] = 'Beendet';
 $lang['VAR_VERSION'] = 'Version';
 $lang['VAR_UNBAN'] = 'Entbannen';
-$lang['VAR_DELETEBAN'] = 'Ban löschen';
+$lang['VAR_DELETEBAN'] = 'Ban l&ouml;schen';
 $lang['VAR_UNMUTE'] = 'Stummschaltung aufheben';
-$lang['VAR_DELETEMUTE'] = 'Stummschaltung löschen';
-$lang['VAR_DELETEKICK'] = 'Kick löschen';
+$lang['VAR_DELETEMUTE'] = 'Stummschaltung l&ouml;schen';
+$lang['VAR_DELETEKICK'] = 'Kick l&ouml;schen';
 $lang['VAR_UNWARN'] = 'Entwarnen';
-$lang['VAR_DELETEWARN'] = 'Warnung löschen';
+$lang['VAR_DELETEWARN'] = 'Warnung l&ouml;schen';
 $lang['VAR_MESSAGE'] = 'Nachricht';
 $lang['VAR_SERVER'] = 'Server';
 $lang['VAR_TICKET'] = 'Ticket';
@@ -202,7 +218,7 @@ $lang['VAR_CREATOR'] = 'Ersteller';
 $lang['VAR_ON'] = 'An';
 $lang['VAR_OFF'] = 'Aus';
 $lang['VAR_ICON'] = 'Icon';
-$lang['VAR_DELETE'] = 'Löschen';
+$lang['VAR_DELETE'] = 'L&ouml;schen';
 $lang['VAR_DEFAULT'] = 'Voreinstellung';
 $lang['VAR_ACTION'] = 'Aktion';
 $lang['VAR_PASSWORD'] = 'Passwort';
@@ -211,8 +227,8 @@ $lang['VAR_GLOBAL'] = 'Global';
 $lang['VAR_SAVE'] = 'Speichern';
 $lang['VAR_CANCEL'] = 'Abbrchen';
 $lang['VAR_MONTHLY'] = 'Monatlich';
-$lang['VAR_WEEKLY'] = 'Wöchentlich';
-$lang['VAR_DAILY'] = 'Täglich';
+$lang['VAR_WEEKLY'] = 'W&ouml;chentlich';
+$lang['VAR_DAILY'] = 'T&auml;glich';
 $lang['VAR_TODAY'] = 'Heute';
 $lang['VAR_THISWEEK'] = 'Diese Woche';
 $lang['VAR_TOTAL'] = 'Total';
@@ -220,14 +236,15 @@ $lang['VAR_LOADING'] = 'Lade...';
 $lang['VAR_BY'] = 'von';
 $lang['VAR_ALL'] = 'alles';
 $lang['VAR_UNKNOWN'] = 'Unbekannt';
-$lang['VAR_BACK'] = 'Zurück';
+$lang['VAR_BACK'] = 'Zur&uuml;ck';
 $lang['VAR_ACTIVE'] = 'Aktiv';
 $lang['VAR_INACTIVE'] = 'Inaktiv';
 $lang['VAR_TRUE'] = 'Wahr';
 $lang['VAR_FALSE'] = 'Falsch';
 $lang['VAR_NEVER'] = 'Nie';
+$lang['VAR_IP'] = 'IP';
 
 $lang['TEXT_NORESULTS'] = 'Es konnten keine Ergebnisse angezeigt werden.';
-$lang['TEXT_NORESULTS_SEARCH'] = 'Keine Ergebnisse gefunden für';
-$lang['TEXT_SELECTOPTION'] = ' -- Option auswählen -- ';
-$lang['TEXT_NOTASKS'] = 'Keine Aufgaben! Einen schönen Tag noch :)';
+$lang['TEXT_NORESULTS_SEARCH'] = 'Keine Ergebnisse gefunden f&uuml;r';
+$lang['TEXT_SELECTOPTION'] = ' -- Option ausw&auml;hlen -- ';
+$lang['TEXT_NOTASKS'] = 'Keine Aufgaben! Einen sch&ouml;nen Tag noch :)';
