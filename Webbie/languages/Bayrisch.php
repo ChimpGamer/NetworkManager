2fb787089556b287586f8db8cd10e743cd848c53
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2018-03-13 16:54:11 +0100

    Add files via upload

---
+++ Webbie/languages/Bayrisch.php
@@ -0,0 +1,233 @@
+<?php //Last updated 05.03.2018
+$lang = array();
+
+$lang['LANGUAGE'] = 'Bayrisch/Boarisch - Wer moag a Bier?';
+$lang['AUTHOR'] = 'Player_Schark';
+
+$lang['TITLE_OVERVIEW'] = 'Schwarzes Brett';
+$lang['TITLE_ANALYTICS'] = 'Deppometer';
+
+$lang['TITLE_PLAYERS'] = 'Deppn';
+$lang['TITLE_PUNISHMENTS'] = 'Bestrofunga';
+$lang['TITLE_REPORTS'] = 'Gmeldete Deppn';
+$lang['TITLE_CHAT'] = 'Biergoadn';
+$lang['TITLE_CHATLOGS'] = 'Aufgschiamner Biergoadn';
+$lang['TITLE_HELPOP'] = 'Huifecenter';
+$lang['TITLE_TICKETS'] = 'Huife';
+
+$lang['TITLE_SETTINGS'] = 'Oanstellunga';
+$lang['TITLE_MOTD'] = 'NDT';
+$lang['TITLE_PERMISSIONS'] = 'Erlaubnisse';
+$lang['TITLE_TAGS'] = 'Tags';
+$lang['TITLE_FILTER'] = 'Verbodene Wörder';
+$lang['TITLE_COMMANDBLOCKER'] = 'Des deafst du net doa';
+$lang['TITLE_ANNOUNCEMENTS'] = 'Ausrufe';
+$lang['TITLE_TABCOMPLETECOMMANDS'] = 'Selbergmoachte dinga';
+
+$lang['TITLE_GROUPSANDACCOUNTS'] = 'Gruppen & Accounts';
+
+$lang['TITLE_TASKS'] = 'Späder moacha';
+$lang['TITLE_ACCOUNTSETTINGS'] = 'Account Oanstellunga';
+$lang['TITLE_LOGOUT'] = 'Geh weg';
+
+$lang['TITLE_DASHBOARD'] = 'Schwarzes Brett';
+$lang['TITLE_NETWORK'] = 'Mei Server';
+$lang['TITLE_NETWORKSETTINGS'] = 'Mei Server Oanstellunga';
+$lang['TITLE_ADMINISTRATION'] = 'Kinigshaus';
+$lang['TITLE_ADDONS'] = 'Biergerdn';
+$lang['TITLE_ACCOUNT'] = 'Account';
+
+$lang['TITLE_MOTDANDMAINTENANCE'] = 'NDT & Aufbau';
+
+$lang['DASHBOARD_TOTALPLAYERS'] = 'Alle Deppn';
+$lang['DASHBOARD_TODAYONLINEPLAYERS'] = 'Heidige Deppn';
+$lang['DASHBOARD_TODAYSNEWPLAYERS'] = 'Heidige neie Deppn';
+$lang['DASHBOARD_TODAYSPLAYTIME'] = 'Heutige Saufzeit';
+$lang['DASHBOARD_PLAYERSTATISTICS'] = 'Deppometer';
+$lang['DASHBOARD_NEWESTPLAYERS'] = 'Neiaste Deppn';
+
+$lang['ANALYTICS_ONLINE'] = 'Läuft';
+$lang['ANALYTICS_AVERAGESTATS'] = 'Allgemeine Sauferei';
+$lang['ANALYTICS_MOSTPLAYEDVERSIONS'] = 'Bestes Bier';
+$lang['ANALYTICS_PLAYERREGIONS'] = 'Dörfer';
+$lang['ANALYTICS_PLAYTIME'] = 'Saufzeit';
+$lang['ANALYTICS_USERRETENTION'] = 'Nutzerbindung';
+$lang['ANALYTICS_FUTUREPORJECTIONS'] = 'Kommender Bierverkauf';
+
+$lang['PLAYER_PLAYERINFORMATION'] = 'Deppninfo';
+$lang['PLAYER_PLAYERSTATISTICS'] = 'Deppninfo';
+$lang['PLAYER_LATESTSESSIONS'] = 'Letzter Suff';
+$lang['PLAYER_NOTES'] = 'O gschriemnes';
+$lang['PLAYER_PUNISHMENTS'] = 'Bestrofunga';
+$lang['PLAYER_USERNAME'] = 'Name';
+$lang['PLAYER_NICKNAME'] = 'Biergoadn Name';
+$lang['PLAYER_UUID'] = 'UUID';
+$lang['PLAYER_COUNTRY'] = 'Dorf';
+$lang['PLAYER_LATESTMINECRAFTVERSION'] = 'Neiastes Dorf';
+$lang['PLAYER_IPADDRESS'] = 'IP';
+$lang['PLAYER_JOINED'] = 'Hoad n Biergoadn bedredn am';
+$lang['PLAYER_LASTLOGIN'] = 'Letzter Suff';
+$lang['PLAYER_LASTLOGOUT'] = 'Letzte Kantn';
+$lang['PLAYER_ONLINE'] = 'Is am Saufn';
+$lang['PLAYER_PLAYTIME'] = 'Saufzeit';
+$lang['PLAYER_AVERAGEPLAYTIME'] = 'Durchschnittliche Saufzeit';
+$lang['PLAYER_JOINSAT'] = 'Normalerweiser Sauft der so ab';
+$lang['PLAYER_ALTACCOUNTS'] = 'Seine Freinde';
+$lang['PLAYER_SEARCH'] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+
+$lang['PUNISHMENTS_PUNISHMENT'] = 'Bestrofunga';
+$lang['PUNISHMENTS_TYPE'] = 'Verdiende Bestrofung';
+$lang['PUNISHMENTS_PUNISHED'] = 'Bestroaft';
+$lang['PUNISHMENTS_PUNISHER'] = 'Schläger';
+$lang['PUNISHMENTS_TIME'] = 'Verdiende dauer';
+$lang['PUNISHMENTS_END'] = 'Bis doahin';
+$lang['PUNISHMENTS_SERVER'] = 'Server';
+$lang['PUNISHMENTS_REASON'] = 'Verdiender Gruand';
+$lang['PUNISHMENTS_STATUS'] = 'Status';
+$lang['PUNISHMENTS_UNBANNER'] = 'Spielverderber';
+$lang['PUNISHMENTS_ACTIVE'] = 'Läuft';
+$lang['PUNISHMENTS_EXPIRED'] = 'Verfallen';
+$lang['PUNISHMENTS_SEARCH'] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+
+$lang['REPORTS_REPORT'] = 'Drum ding';
+$lang['REPORTS_REPORTER'] = 'Saubreis';
+$lang['REPORTS_CLOSE'] = 'Beendet';
+$lang['REPORTS_SEARCH'] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+
+$lang['CHAT_SEARCH'] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+
+$lang['NOTES_NOTE'] = 'Notiz';
+
+$lang['CHATLOG_CHATLOGID'] = 'Chatlog ID';
+$lang['CHATLOG_TRACKED'] = 'Depp';
+$lang['CHATLOG_CHATLOGS'] = 'Gspeicherte Sprüche';
+$lang['CHATLOG_SEARCH'] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+
+$lang['HELPOP_REQUESTER'] = 'Depp der Huife braucht';
+$lang['HELPOP_SEARCH'] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+
+$lang['TICKET_TICKET'] = 'Ticket';
+$lang['TICKET_TITLE'] = 'Titl';
+$lang['TICKET_ASSIGNEDTO'] = 'Zuagewiesen o';
+$lang['TICKET_LASTUPDATE'] = 'Zuletzt ogschaud';
+$lang['TICKET_ANSWER'] = 'Schreib eam';
+$lang['TICKET_CLOSE'] = 'Moachs zua';
+$lang["TICKET_SEARCH"] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+$lang['TICKET_CREATED'] = 'Gmoacht';
+$lang['TICKET_PRIORITY'] = 'Priorität';
+$lang['TICKET_PRIORITY_LOW'] = 'Scheiß egal';
+$lang['TICKET_PRIORITY_MEDIUM'] = 'Basst scho';
+$lang['TICKET_PRIORITY_HIGH'] = 'Wichtig';
+$lang['TICKET_PRIORITY_NO_PRIORITY'] = 'Is mir Egal'; 
+
+$lang['SETTINGS_SEARCH'] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+
+$lang['MOTD_MOTDGENERATOR'] = 'NDT Generator';
+$lang['MOTD_KICKMSG'] = 'Rausschmiss ruf';
+$lang['MOTD_PREVIEW'] = 'Sowas kimmt';
+$lang['MOTD_MAINTENANCEMODE'] = 'Braumodus';
+
+$lang['PERMISSIONS_GROUP_NAME'] = 'Name';
+$lang['PERMISSIONS_GROUP_LADDER'] = 'Karriere';
+$lang['PERMISSIONS_GROUP_RANK'] = 'Stufe';
+$lang['PERMISSIONS_PERMISSIONS_WORLD'] = 'Biergoadn';
+$lang['PERMISSIONS_EXPIRES'] = 'Beendet';
+$lang['PERMISSIONS_PREFIX'] = 'Prefix';
+$lang['PERMISSIONS_SUFFIX'] = 'Suffix';
+$lang['PERMISSIONS_ISDEFAULT'] = 'is Standart';
+$lang['PERMISSIONS_PERMISSION'] = 'Erlaubnisse';
+$lang['PERMISSIONS_GROUP_CREATE'] = 'Erstell a Gruppn';
+$lang['PERMISSIONS_GROUP_PREFIX_ADD'] = 'füg an Prefix dazu';
+$lang['PERMISSIONS_GROUP_SUFFIX_ADD'] = 'füg an Suffix dazu';
+$lang['PERMISSIONS_PERMISSION_ADD'] = 'füg a Erlaubniss dazu';
+$lang['PERMISSIONS_GROUP_MEMBER_ADD'] = 'füg an Deppn dazu';
+$lang['PERMISSIONS_GROUP_MEMBER_LIST'] = 'Zeig alle Deppn';
+$lang['PERMISSIONS_GROUP_SEARCH'] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+$lang['PERMISSIONS_PLAYER_SEARCH'] = 'Suach einfach irgndwaos wirst scho seng obs basst';
+
+$lang['TAGS_TAG_NAME'] = 'Name';
+$lang['TAGS_TAG_TAG'] = 'Tag';
+$lang['TAGS_TAG_DESCRIPTION'] = 'Tag vom Tag';
+$lang['TAGS_TAG_CREATE'] = 'Moach an Tag';
+
+$lang['FILTER_ADDWORD'] = 'Wort hinzufügen';
+
+$lang['COMMANDBLOCKER_ADDCOMMAND'] = 'füg an Befehl dazu';
+
+$lang['ANNOUNCEMENTS_TYPE'] = 'Typ';
+$lang['ANNOUNCEMENTS_MESSAGE'] = 'Noachricht';
+$lang['ANNOUNCEMENTS_CREATE_ANNOUNCEMENT'] = 'Moach a Noachricht';
+
+$lang['TABCOMPLETECOMMANDS_ADDCOMMAND'] = 'füg an Be dazu';
+
+$lang['GROUPANDACCOUNTS_GROUP'] = 'Gruppe';
+$lang['GROUPANDACCOUNTS_USER'] = 'Depp';
+
+$lang['TASKS_YOURTASKS'] = 'Des woas du no maoacha musst';
+
+$lang['ACCOUNTSETTINGS_OLDPASS'] = 'Oids Passwort';
+$lang['ACCOUNTSETTINGS_NEWPASS'] = 'Neis Passwort';
+$lang['ACCOUNTSETTINGS_SELECTED_LANG'] = 'Deine Sproache:';
+
+$lang['LOGIN_TITLE'] = 'NetworkManager #Bayern \(^_^)/';
+$lang['LOGIN_USERNAME'] = 'Dei Noame';
+$lang['LOGIN_PASSWORD'] = 'Passwort';
+$lang['LOGIN_REMEMBERME'] = 'Bleib drinnad';
+$lang['LOGIN_LOGIN'] = 'Ge eine';
+$lang['LOGIN_WRONGPASSWORD'] = 'Du hoast des Foische Passwort';
+$lang['LOGIN_LOGGINGIN'] = 'Logge ein...';
+$lang['LOGIN_NODBCONNECTION'] = 'Die Datenbank verbindung gead ned!';
+
+$lang['VAR_ID'] = 'ID';
+$lang['VAR_VIEW'] = 'Ansicht';
+$lang['VAR_PUNISHMENT'] = 'Bestrofung';
+$lang['VAR_PLAYER'] = 'Depp';
+$lang['VAR_PUNISHER'] = 'Bestroafta';
+$lang['VAR_REASON'] = 'Grund';
+$lang['VAR_TIME'] = 'Zeid';
+$lang['VAR_STARTED'] = 'Läuft';
+$lang['VAR_ENDED'] = 'Hoid';
+$lang['VAR_VERSION'] = 'Version';
+$lang['VAR_UNBAN'] = 'Entbannen';
+$lang['VAR_DELETEBAN'] = 'Ban löschn';
+$lang['VAR_UNMUTE'] = 'Ruhe ausmoacha';
+$lang['VAR_DELETEMUTE'] = 'Ruhe löschn';
+$lang['VAR_DELETEKICK'] = 'Kick löschn';
+$lang['VAR_UNWARN'] = 'Entwoarnen';
+$lang['VAR_DELETEWARN'] = 'Woarnung löschn';
+$lang['VAR_MESSAGE'] = 'Noachricht';
+$lang['VAR_SERVER'] = 'Server';
+$lang['VAR_TICKET'] = 'Ticket';
+$lang['VAR_CREATOR'] = 'Ersteller';
+$lang['VAR_ON'] = 'O';
+$lang['VAR_OFF'] = 'Aus';
+$lang['VAR_ICON'] = 'Buidl';
+$lang['VAR_DELETE'] = 'Löschn';
+$lang['VAR_DEFAULT'] = 'Voroanstellunga';
+$lang['VAR_ACTION'] = 'Aktion';
+$lang['VAR_PASSWORD'] = 'Passwort';
+$lang['VAR_LANGUAGES'] = 'Sprachn';
+$lang['VAR_GLOBAL'] = 'Global';
+$lang['VAR_SAVE'] = 'Spoachen';
+$lang['VAR_CANCEL'] = 'Stoppn';
+$lang['VAR_MONTHLY'] = 'Monatlich';
+$lang['VAR_WEEKLY'] = 'Wöchentlich';
+$lang['VAR_DAILY'] = 'Täglich';
+$lang['VAR_TODAY'] = 'Heide';
+$lang['VAR_THISWEEK'] = 'Die Woche';
+$lang['VAR_TOTAL'] = 'Total';
+$lang['VAR_LOADING'] = 'Lade...';
+$lang['VAR_BY'] = 'von';
+$lang['VAR_ALL'] = 'ois';
+$lang['VAR_UNKNOWN'] = 'Kenn i net';
+$lang['VAR_BACK'] = 'Zruck';
+$lang['VAR_ACTIVE'] = 'Aktiv';
+$lang['VAR_INACTIVE'] = 'Inaktiv';
+$lang['VAR_TRUE'] = 'Stimmt';
+$lang['VAR_FALSE'] = 'Foisch';
+$lang['VAR_NEVER'] = 'Nia';
+
+$lang['TEXT_NORESULTS'] = 'Da kommt nix gscheids raus';
+$lang['TEXT_NORESULTS_SEARCH'] = 'Nix gfunna';
+$lang['TEXT_SELECTOPTION'] = ' -- Wähl woas -- ';
+$lang['TEXT_NOTASKS'] = 'Nix zum doa... Geh ma was saufa \(^_^)/';
