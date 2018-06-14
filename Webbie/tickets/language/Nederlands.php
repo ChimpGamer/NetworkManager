6d389f298094a324febf99277e805e4b061067a6
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2018-06-14 21:39:39 +0200

    Create Nederlands.php

---
+++ Webbie/tickets/language/Nederlands.php
@@ -0,0 +1,26 @@
+<?php //Last updated 15-1-17
+$lang = array();
+
+$lang['LANGUAGE'] = 'Nederlands';
+$lang['AUTHOR'] = 'ChimpGamer';
+
+$lang['HOME_TITLE'] = 'Welkom terug,';
+$lang['HOME_NO_TICKETS'] = "Je hebt nog geen tickets gemaakt. Klik <a href='create.php'>hier</a> om er een te maken.";
+$lang['HOME_TICKET'] = 'Ticket';
+$lang['HOME_LAST_RESPONSE'] = 'Laatste reactie';
+$lang['HOME_STATUS'] = 'Status';
+$lang['HOME_ACTION'] = 'Actie';
+
+$lang['MENU_HOME'] = 'Home';
+$lang['MENU_TICKETS'] = 'Tickets';
+$lang['MENU_TICKETS_CREATETICKET'] = 'Creeër Ticket';
+$lang['MENU_LOGOUT'] = 'Uitloggen';
+
+$lang['TICKET_CREATE_TITLE'] = 'Titel';
+$lang['TICKET_CREATE_MESSAGE'] = 'Bericht';
+$lang['TICKET_CREATE_SUBMIT'] = 'Verstuur';
+
+$lang['VAR_NO_RESPONSE'] = 'Geen reactie';
+$lang['VAR_OPEN'] = 'OPEN';
+$lang['VAR_CLOSED'] = 'GESLOTEN';
+$lang['VAR_VIEW'] = 'bekijk';
