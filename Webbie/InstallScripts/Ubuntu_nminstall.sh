242b3309679239615176c5b2ba6c5bb27a68511e
ChimpGamer <Tim.Cremers@Ziggo.nl>
2019-08-09 13:04:16 +0200

    Update Ubuntu_nminstall.sh
    Updated to php 7.3 and latest changes of the web interface

--- Webbie/InstallScripts/Ubuntu_nminstall.sh
+++ Webbie/InstallScripts/Ubuntu_nminstall.sh
@@ -16,7 +16,7 @@ add-apt-repository ppa:ondrej/php
 apt-get update
 apt-get install -y sudo apache2 nano zip
 #
-apt-get install -y php7.2 php7.2-cli php7.2-cgi php7.2-curl php7.2-mysql php7.2-common php7.2-pdo php7.2-xml php7.2-mbstring libapache2-mod-php7.2
+apt-get install -y php7.3 php7.3-cli php7.3-cgi php7.3-curl php7.3-mysql php7.3-common php7.3-pdo php7.3-xml php7.3-mbstring libapache2-mod-php7.3
 #
 a2enmod rewrite
 service apache2 restart
@@ -25,8 +25,8 @@ wget -O webbie.zip $downloadlink
 mkdir $directory
 unzip /tmp/webbie.zip -d $directory
 #
-chmod 777 $directory/inc/php/dep/protected/config.php
-chmod 777 $directory/inc/json/protected/settings.json
+chmod 777 $directory/protected/config.ini
+chmod 777 $directory/protected/settings.json
 chmod 777 -R $directory/inc/php/dep/languages
 #
 exit
