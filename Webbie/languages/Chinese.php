1cb0a032e5b7616927c1d163738e5214b6d67473
ChimpGamer <Tim.Cremers@Ziggo.nl>
2021-05-07 20:05:01 +0200

    Add files via upload

---
+++ Webbie/languages/Chinese.php
@@ -0,0 +1,269 @@
+<?php //Last updated 28-12-2018
+$lang = array();
+
+$lang['LANGUAGE'] = 'Chinese';
+$lang['THEME'] = 'Default';
+$lang['AUTHOR'] = 'SummerGoro';
+
+$lang['PANEL_TITLE'] = '网页管理器';
+
+$lang['TITLE_OVERVIEW'] = '概览';
+$lang['TITLE_ANALYTICS'] = '分析';
+
+$lang['TITLE_PLAYERS'] = '玩家';
+$lang['TITLE_PUNISHMENTS'] = '封禁';
+$lang['TITLE_REPORTS'] = '报告';
+$lang['TITLE_CHAT'] = '聊天';
+$lang['TITLE_CHATLOGS'] = '聊天记录';
+$lang['TITLE_HELPOP'] = 'Helpop';
+$lang['TITLE_TICKETS'] = 'Tickets';
+
+$lang['TITLE_SETTINGS'] = '设置';
+$lang['TITLE_LANGUAGES'] = '语言';
+$lang['TITLE_MOTD'] = 'MOTD';
+$lang['TITLE_PERMISSIONS'] = '权限';
+$lang['TITLE_SERVERS'] = '服务器';
+$lang['TITLE_TAGS'] = '标签';
+$lang['TITLE_FILTER'] = '筛选';
+$lang['TITLE_COMMANDBLOCKER'] = '指令黑名单';
+$lang['TITLE_ANNOUNCEMENTS'] = '公告';
+$lang['TITLE_TABCOMPLETECOMMANDS'] = 'TAB补全指令';
+
+$lang['TITLE_GROUPSANDACCOUNTS'] = '权限组 & 账户';
+
+$lang['TITLE_TASKS'] = '任务';
+$lang['TITLE_ACCOUNTSETTINGS'] = '用户设置';
+$lang['TITLE_LOGOUT'] = '登出';
+
+$lang['TITLE_DASHBOARD'] = '仪表板';
+$lang['TITLE_NETWORK'] = '网络';
+$lang['TITLE_NETWORKSETTINGS'] = '网络设置';
+$lang['TITLE_ADMINISTRATION'] = '管理权限';
+$lang['TITLE_ADDONS'] = 'Addons';
+$lang['TITLE_ACCOUNT'] = '账户';
+
+$lang['TITLE_MOTDANDMAINTENANCE'] = 'MOTD & 维护';
+
+$lang['DASHBOARD_TOTALPLAYERS'] = '总玩家';
+$lang['DASHBOARD_TODAYONLINEPLAYERS'] = '今天在线玩家';
+$lang['DASHBOARD_TODAYSNEWPLAYERS'] = '今天新增玩家';
+$lang['DASHBOARD_TODAYSPLAYTIME'] = '今天游玩时间';
+$lang['DASHBOARD_PLAYERSTATISTICS'] = '玩家统计数据';
+$lang['DASHBOARD_NEWESTPLAYERS'] = '玩家分布地图';
+$lang['DASHBOARD_PLAYERSTATISTICS_TOOLTIP'] = '显示过去60天内有用的玩家人数统计信息 ';
+$lang['DASHBOARD_NEWESTPLAYERS_TOOLTOP'] = '显示地图，其中包含最近60天加入的玩家的位置';
+
+$lang['ANALYTICS_ONLINE'] = '在线';
+$lang['ANALYTICS_AVERAGESTATS'] = '平均统计';
+$lang['ANALYTICS_MOSTPLAYEDVERSIONS'] = '最多使用的版本';
+$lang['ANALYTICS_PLAYERREGIONS'] = '玩家地区';
+$lang['ANALYTICS_PLAYTIME'] = '游玩时间';
+$lang['ANALYTICS_USERRETENTION'] = '用户保留';
+$lang['ANALYTICS_FUTUREPORJECTIONS'] = '未来预测';
+$lang['ANALYTICS_ONLINE_TOOLTOP'] = '历来在线玩家的图表';
+$lang['ANALYTICS_AVERAGESTATS_TOOLTOP'] = 'ANPPD（每日平均新玩家）和ARPPD（每日平均返回玩家）按月，每周和每日显示统计信息';
+$lang['ANALYTICS_MOSTPLAYEDVERSIONS_TOOLTIP'] = '以数量和百分比显示玩家的Minecraft游玩次数最多的版本';
+$lang['ANALYTICS_PLAYERREGIONS_TOOLTIP'] = '在条形图和地图中显示您的玩家来自哪个国家';
+$lang['ANALYTICS_PLAYTIME_TOOLTIP'] = '显示您今天，本周和本周网络上的总游玩时间';
+$lang['ANALYTICS_USERRETENTION_TOOLTIP'] = '首次加入后返回的玩家百分比。天数显示。';
+$lang['ANALYTICS_FUTUREPORJECTIONS_TOOLTIP'] = '该算法将向您展示新玩家和活跃玩家的最终未来预测';
+
+$lang['PLAYER_PLAYERINFORMATION'] = '玩家信息';
+$lang['PLAYER_PLAYERSTATISTICS'] = '玩家状态';
+$lang['PLAYER_LATESTSESSIONS'] = '登录记录';
+$lang['PLAYER_NOTES'] = '笔记';
+$lang['PLAYER_PUNISHMENTS'] = '封禁记录';
+$lang['PLAYER_USERNAME'] = '玩家ID';
+$lang['PLAYER_NICKNAME'] = '昵称';
+$lang['PLAYER_UUID'] = 'UUID';
+$lang['PLAYER_COUNTRY'] = '国家';
+$lang['PLAYER_LATESTMINECRAFTVERSION'] = '最新使用版本';
+$lang['PLAYER_IPADDRESS'] = 'IP地址';
+$lang['PLAYER_JOINED'] = '加入时间';
+$lang['PLAYER_LASTLOGIN'] = '最新加入时间';
+$lang['PLAYER_LASTLOGOUT'] = '最新登出时间';
+$lang['PLAYER_ONLINE'] = '在线状态';
+$lang['PLAYER_PLAYTIME'] = '游玩时间';
+$lang['PLAYER_AVERAGEPLAYTIME'] = '平均游玩时间';
+$lang['PLAYER_JOINSAT'] = '玩家通常加入';
+$lang['PLAYER_ALTACCOUNTS'] = '其他帐户';
+$lang['PLAYER_SEARCH'] = '通过ID,IP,UUID搜寻玩家';
+$lang['PLAYER_PREMIUM'] = 'Premium';
+
+$lang['PUNISHMENTS_PUNISHMENT'] = '封禁状态';
+$lang['PUNISHMENTS_ID'] = 'ID';
+$lang['PUNISHMENTS_TYPE'] = '类型';
+$lang['PUNISHMENTS_PUNISHED'] = '被封禁玩家';
+$lang['PUNISHMENTS_PUNISHER'] = '封禁人';
+$lang['PUNISHMENTS_TIME'] = '时间';
+$lang['PUNISHMENTS_END'] = '结束';
+$lang['PUNISHMENTS_SILENT'] = 'Silent';
+$lang['PUNISHMENTS_STATUS'] = '状态';
+$lang['PUNISHMENTS_UNBANNER'] = 'Unbanner';
+$lang['PUNISHMENTS_ACTIVE'] = 'Active';
+$lang['PUNISHMENTS_EXPIRED'] = '已到期';
+$lang['PUNISHMENTS_SEARCH'] = '通过玩家，封禁人，IP或原因搜寻被封禁玩家';
+
+$lang['REPORTS_REPORT'] = '报告';
+$lang['REPORTS_REPORTER'] = '报告人';
+$lang['REPORTS_CLOSE'] = '关闭';
+$lang['REPORTS_CLOSED_BY'] = '已关闭 来自';
+$lang['REPORTS_SEARCH'] = '通过玩家，报告人或原因搜寻报告';
+
+$lang['CHAT_SEARCH'] = '通过玩家名称，uuid，消息，服务器或IP搜寻聊天记录';
+$lang['CHAT_TYPE'] = "类型";
+
+$lang['NOTES_NOTE'] = '笔记';
+
+$lang['CHATLOG_CHATLOGID'] = '消息日志 ID';
+$lang['CHATLOG_TRACKED'] = '已追踪';
+$lang['CHATLOG_CHATLOGS'] = '消息日志';
+$lang['CHATLOG_SEARCH'] = '按创建者，UUID或跟踪搜索聊天记录';
+
+$lang['HELPOP_REQUESTER'] = '请求人';
+$lang['HELPOP_SEARCH'] = '通过请求者，服务器或消息搜索帮助请求';
+
+$lang['TICKET_TICKET'] = '票';
+$lang['TICKET_TITLE'] = '标题';
+$lang['TICKET_ASSIGNEDTO'] = '分配';
+$lang['TICKET_LASTUPDATE'] = '上次更新';
+$lang['TICKET_ANSWER'] = '回复';
+$lang['TICKET_CLOSE'] = '关闭';
+$lang["TICKET_SEARCH"] = '按创建者，标题或分配给';
+$lang['TICKET_CREATED'] = '创建';
+$lang['TICKET_PRIORITY'] = '优先';
+$lang['TICKET_PRIORITY_LOW'] = '低';
+$lang['TICKET_PRIORITY_MEDIUM'] = '中';
+$lang['TICKET_PRIORITY_HIGH'] = '高';
+$lang['TICKET_PRIORITY_NO_PRIORITY'] = '没有优先权';
+
+$lang['SETTINGS_SEARCH'] = '按变量搜索设置';
+
+$lang['LANGUAGE_NAME'] = '名称';
+
+$lang['MOTD_MOTDGENERATOR'] = 'MOTD管理器';
+$lang['MOTD_KICKMSG'] = '踢出消息';
+$lang['MOTD_PREVIEW'] = '预览';
+$lang['MOTD_MAINTENANCEMODE'] = '维护模式';
+
+$lang['PERMISSIONS_GROUP_NAME'] = '名称';
+$lang['PERMISSIONS_GROUP_LADDER'] = 'Ladder';
+$lang['PERMISSIONS_GROUP_RANK'] = '权重';
+$lang['PERMISSIONS_PERMISSIONS_WORLD'] = '世界';
+$lang['PERMISSIONS_EXPIRES'] = '过期';
+$lang['PERMISSIONS_PREFIX'] = '前缀';
+$lang['PERMISSIONS_SUFFIX'] = '后缀';
+$lang['PERMISSIONS_ISDEFAULT'] = '默认';
+$lang['PERMISSIONS_PERMISSION'] = '权限';
+$lang['PERMISSIONS_GROUP_CREATE'] = '创建权限组';
+$lang['PERMISSIONS_GROUP_PARENT_ADD'] = '添加父级';
+$lang['PERMISSIONS_GROUP_PREFIX_ADD'] = '添加前缀';
+$lang['PERMISSIONS_GROUP_SUFFIX_ADD'] = '添加后缀';
+$lang['PERMISSIONS_PERMISSION_ADD'] = '添加权限';
+$lang['PERMISSIONS_GROUP_MEMBER_ADD'] = '添加成员';
+$lang['PERMISSIONS_GROUP_MEMBER_LIST'] = '成员列表';
+$lang['PERMISSIONS_GROUP_SEARCH'] = '通过ID或名称搜寻群组';
+$lang['PERMISSIONS_PLAYER_SEARCH'] = '通过ID，前缀或后缀或名称搜寻玩家';
+
+$lang['SERVERS_DISPLAYNAME'] = '显示名称';
+$lang['SERVERS_PORT'] = '端口';
+$lang['SERVERS_MOTD'] = 'Motd';
+$lang['SERVERS_RESTRICTED'] = 'Restricted';
+$lang['SERVERS_ISLOBBY'] = 'isLobby';
+$lang['SERVERS_ONLINE'] = '在线';
+$lang['SERVERS_ADD_SERVER'] = '添加服务器';
+
+$lang['TAGS_TAG_NAME'] = '名称';
+$lang['TAGS_TAG_TAG'] = '标签';
+$lang['TAGS_TAG_DESCRIPTION'] = '描述';
+$lang['TAGS_TAG_CREATE'] = '添加标签';
+$lang['TAGS_SEARCH'] = '通过ID或标签搜寻';
+
+$lang['FILTER_ADDWORD'] = '添加单词';
+
+$lang['COMMANDBLOCKER_ADDCOMMAND'] = '添加指令';
+
+$lang['ANNOUNCEMENTS_TYPE'] = '类型';
+$lang['ANNOUNCEMENTS_MESSAGE'] = '信息';
+$lang['ANNOUNCEMENTS_SOUND'] = '声音';
+$lang['ANNOUNCEMENTS_CREATE_ANNOUNCEMENT'] = '创建公告';
+
+$lang['TABCOMPLETECOMMANDS_ADDCOMMAND'] = '添加命令';
+
+$lang['GROUPANDACCOUNTS_GROUP'] = '群组';
+$lang['GROUPANDACCOUNTS_USER'] = '用户';
+
+$lang['TASKS_YOURTASKS'] = '你的任务';
+
+$lang['ACCOUNTSETTINGS_OLDPASS'] = '旧密码';
+$lang['ACCOUNTSETTINGS_NEWPASS'] = '新密码';
+$lang['ACCOUNTSETTINGS_SELECTED_LANG'] = '选择语言:';
+$lang['ACCOUNTSETTINGS_SELECTED_THEME'] = 'S选择主题:';
+
+$lang['LOGIN_TITLE'] = '<b>网页</b>管理器';
+$lang['LOGIN_USERNAME'] = '用户名';
+$lang['LOGIN_PASSWORD'] = '密码';
+$lang['LOGIN_REMEMBERME'] = '记住我';
+$lang['LOGIN_LOGIN'] = '登入';
+$lang['LOGIN_WRONGPASSWORD'] = '错误的密码';
+$lang['LOGIN_LOGGINGIN'] = '登陆成功，请稍等...';
+$lang['LOGIN_NODBCONNECTION'] = '无法连接到数据库';
+
+$lang['VAR_ID'] = 'ID';
+$lang['VAR_VIEW'] = '显示';
+$lang['VAR_PUNISHMENT'] = '封禁';
+$lang['VAR_PLAYER'] = '玩家';
+$lang['VAR_PUNISHER'] = '封禁者';
+$lang['VAR_REASON'] = '原因';
+$lang['VAR_TIME'] = '在线时长';
+$lang['VAR_STARTED'] = '登录时间';
+$lang['VAR_ENDED'] = '登出时间';
+$lang['VAR_VERSION'] = '版本';
+$lang['VAR_UNBAN'] = '解除ban';
+$lang['VAR_DELETEBAN'] = '删除BAN';
+$lang['VAR_UNMUTE'] = 'UNMUTE';
+$lang['VAR_DELETEMUTE'] = '删除 MUTE';
+$lang['VAR_DELETEKICK'] = '删除 KICK';
+$lang['VAR_UNWARN'] = 'UNWARN';
+$lang['VAR_DELETEWARN'] = '删除 WARN';
+$lang['VAR_MESSAGE'] = '信息';
+$lang['VAR_HOVERMESSAGE'] = '悬停信息';
+$lang['VAR_SERVER'] = '服务器';
+$lang['VAR_TICKET'] = '票';
+$lang['VAR_CREATOR'] = '创建人';
+$lang['VAR_ON'] = '开';
+$lang['VAR_OFF'] = '关';
+$lang['VAR_ICON'] = '图标';
+$lang['VAR_EDIT'] = '编辑';
+$lang['VAR_DELETE'] = '删除';
+$lang['VAR_DEFAULT'] = '默认';
+$lang['VAR_ACTION'] = '动作';
+$lang['VAR_PASSWORD'] = '密码';
+$lang['VAR_LANGUAGES'] = '语言';
+$lang['VAR_THEMES'] = '主题';
+$lang['VAR_GLOBAL'] = '全局';
+$lang['VAR_SAVE'] = '保存';
+$lang['VAR_CANCEL'] = '取消';
+$lang['VAR_MONTHLY'] = '月';
+$lang['VAR_WEEKLY'] = '周';
+$lang['VAR_DAILY'] = '天';
+$lang['VAR_TODAY'] = '今天';
+$lang['VAR_THISWEEK'] = '这周';
+$lang['VAR_TOTAL'] = '全部';
+$lang['VAR_LOADING'] = '加载中...';
+$lang['VAR_BY'] = '来自';
+$lang['VAR_ALL'] = '所有';
+$lang['VAR_UNKNOWN'] = '未知';
+$lang['VAR_BACK'] = '返回';
+$lang['VAR_ACTIVE'] = '活跃';
+$lang['VAR_INACTIVE'] = '不活跃';
+$lang['VAR_TRUE'] = '开启';
+$lang['VAR_FALSE'] = '关闭';
+$lang['VAR_NEVER'] = '永不';
+$lang['VAR_IP'] = 'IP';
+$lang['VAR_DURATION'] = '期间';
+
+$lang['TEXT_NORESULTS'] = '无法显示结果';
+$lang['TEXT_NORESULTS_SEARCH'] = '没有查找到结果';
+$lang['TEXT_SELECTOPTION'] = ' -- 选择一个选项 -- ';
+$lang['TEXT_NOTASKS'] = '没有任务。过个愉快的一天吧 :)';
